1430d28f8b6ce322ebe389ab1a792d3e9dd12f68
javv <vinas.johnandrei@gmail.com>
2026-05-04 12:03:29 +0800

    initial ORM annotations and getters/setters for database entity

---
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/user/RoleJpaEntity.java
@@ -0,0 +1,46 @@
+package com.javv.inventorySystem.infrastructure.persistence.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class RoleJpaEntity {
+  @Id
+  @Column(name = "id", nullable = false)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private int id;
+
+  @Column(name = "name", length = 15, nullable = false)
+  private String name;
+
+  public RoleJpaEntity() {
+  }
+
+  public RoleJpaEntity(
+      int id,
+      String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+}

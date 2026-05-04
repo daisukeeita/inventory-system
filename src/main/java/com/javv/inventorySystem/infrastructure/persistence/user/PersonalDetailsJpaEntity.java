8f3cfc86e983c1f2ffdf67b08c383aedde60526d
javv <vinas.johnandrei@gmail.com>
2026-05-04 12:03:02 +0800

    initial ORM annotations and getters/setters for database entity

---
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/user/PersonalDetailsJpaEntity.java
@@ -0,0 +1,100 @@
+package com.javv.inventorySystem.infrastructure.persistence.user;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "personal_details")
+public class PersonalDetailsJpaEntity {
+  @Id
+  private UUID id;
+
+  @OneToOne
+  @MapsId
+  @JoinColumn(name = "user_id")
+  private UserJpaEntity user;
+
+  @Column(name = "display_name", length = 255, nullable = false)
+  private String displayName;
+
+  @Column(name = "first_name", length = 100, nullable = false)
+  private String firstName;
+
+  @Column(name = "middle_initial", length = 5, nullable = true)
+  private String middleInitial;
+
+  @Column(name = "last_name", length = 100, nullable = false)
+  private String lastName;
+
+  @Column(name = "profile_picture", length = 255, nullable = true)
+  private String profilePicture;
+
+  public PersonalDetailsJpaEntity() {
+  }
+
+  public PersonalDetailsJpaEntity(
+      UserJpaEntity user,
+      String firstName,
+      String middleInitial,
+      String lastName) {
+    this.user = user;
+    this.firstName = firstName;
+    this.middleInitial = middleInitial;
+    this.lastName = lastName;
+  }
+
+  public void setUser(UserJpaEntity user) {
+    this.user = user;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setMiddleInitial(String middleInitial) {
+    this.middleInitial = middleInitial;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setProfilePicture(String profilePicture) {
+    this.profilePicture = profilePicture;
+  }
+
+  public void setDisplayName(String displayName) {
+    this.displayName = displayName;
+  }
+
+  public UUID getUserId() {
+    return id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleInitial() {
+    return middleInitial;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  public String getProfilePicture() {
+    return profilePicture;
+  }
+}

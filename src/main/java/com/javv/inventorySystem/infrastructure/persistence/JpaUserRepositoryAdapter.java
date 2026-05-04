83c9fc63e31229b9a4ec39989d2d37f94e262a37
javv <vinas.johnandrei@gmail.com>
2026-05-04 18:04:28 +0800

    injecting user mapper

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/JpaUserRepositoryAdapter.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/JpaUserRepositoryAdapter.java
@@ -4,16 +4,27 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
+import com.javv.inventorySystem.application.mapper.UserMapper;
 import com.javv.inventorySystem.domain.model.user.User;
 import com.javv.inventorySystem.domain.repository.user.UserRepositoryInterface;
 import com.javv.inventorySystem.infrastructure.persistence.user.UserJpaEntity;
 
+/**
+ * This is where the system "translates" the JPA Entity (User) to Domain Entity (User) after getting
+ * the data from the database.
+ *
+ * <p>This is where the system "translates" the Domain Entity (User) to JPA Entity (User) before
+ * inserting the object to the database.
+ */
 @Repository
 public class JpaUserRepositoryAdapter implements UserRepositoryInterface {
   private final SpringDataJpaUserRepository springDataJpaUserRepository;
+  private final UserMapper userMapper;
 
-  public JpaUserRepositoryAdapter(SpringDataJpaUserRepository springDataJpaUserRepository) {
+  public JpaUserRepositoryAdapter(
+      SpringDataJpaUserRepository springDataJpaUserRepository, UserMapper userMapper) {
     this.springDataJpaUserRepository = springDataJpaUserRepository;
+    this.userMapper = userMapper;
   }
 
   @Override
@@ -22,6 +33,9 @@ public class JpaUserRepositoryAdapter implements UserRepositoryInterface {
 
     User user = new User();
 
-    return user;
+    return null;
   }
+
+  @Override
+  public void save(User user) {}
 }

bac682bed7460c8336c55f6a3e5d868665e66676
javv <vinas.johnandrei@gmail.com>
2026-05-06 12:37:44 +0800

    renamed file and repackaged

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/user/UserJpaRepository.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/user/UserJpaRepository.java
@@ -1,11 +1,11 @@
-package com.javv.inventorySystem.infrastructure.persistence;
+package com.javv.inventorySystem.infrastructure.persistence.user;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpringJpaUserRepository extends JpaRepository<UserJpaEntity, UUID> {
+public interface UserJpaRepository extends JpaRepository<UserJpaEntity, UUID> {
 
   Optional<UserJpaEntity> findByUsername(String username);
 }

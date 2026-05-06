22ed0c82fc0776498c0c5b2ef90b66e80e1d3d61
javv <vinas.johnandrei@gmail.com>
2026-05-06 12:59:34 +0800

    renamed file

---
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/role/RolePersistenceMapper.java
@@ -0,0 +1,20 @@
+package com.javv.inventorySystem.infrastructure.persistence.role;
+
+import com.javv.inventorySystem.domain.model.role.Role;
+
+public class RolePersistenceMapper {
+
+  public Role entityToDomain(RoleJpaEntity roleJpaEntity) {
+    Role role = new Role();
+    role.setId(roleJpaEntity.getId());
+    role.setName(roleJpaEntity.getName());
+    return role;
+  }
+
+  public RoleJpaEntity domainToEntity(Role role) {
+    RoleJpaEntity roleJpaEntity = new RoleJpaEntity();
+    roleJpaEntity.setId(role.getId());
+    roleJpaEntity.setName(role.getName());
+    return roleJpaEntity;
+  }
+}

3c037cea42a912340e6fe9ba58b3980c92194b6d
javv <vinas.johnandrei@gmail.com>
2026-05-06 20:52:12 +0800

    added customed method for retrieval

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/role/RoleJpaRepository.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/role/RoleJpaRepository.java
@@ -1,6 +1,9 @@
 package com.javv.inventorySystem.infrastructure.persistence.role;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface RoleJpaRepository extends CrudRepository<RoleJpaEntity, Integer> {
+  Optional<RoleJpaEntity> findByName(String name);
 }

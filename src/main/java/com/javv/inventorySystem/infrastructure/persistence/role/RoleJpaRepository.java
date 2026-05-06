a22a0cddaf06d91950f301d3437889911054ab65
javv <vinas.johnandrei@gmail.com>
2026-05-06 13:00:02 +0800

    added content

---
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/role/RoleJpaRepository.java
@@ -0,0 +1,6 @@
+package com.javv.inventorySystem.infrastructure.persistence.role;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleJpaRepository extends CrudRepository<RoleJpaEntity, Integer> {
+}

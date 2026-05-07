76b1fd3361dbf43be2ac262a611ff25577e4c9ca
javv <vinas.johnandrei@gmail.com>
2026-05-07 21:40:24 +0800

    moving the exception to adapter

--- src/main/java/com/javv/inventorySystem/application/service/RoleService.java
+++ src/main/java/com/javv/inventorySystem/application/service/RoleService.java
@@ -2,28 +2,24 @@ package com.javv.inventorySystem.application.service;
 
 import java.util.Optional;
 
-import org.springframework.dao.DataAccessResourceFailureException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.javv.inventorySystem.domain.exception.ResourceNotFoundException;
-import com.javv.inventorySystem.domain.exception.RoleAlreadyExistsException;
-import com.javv.inventorySystem.domain.exception.SystemUnavailableException;
 import com.javv.inventorySystem.domain.model.role.Role;
-import com.javv.inventorySystem.infrastructure.persistence.role.RolePersistenceAdapter;
+import com.javv.inventorySystem.domain.repository.RoleRepositoryInterface;
 
 @Service
 @Transactional(readOnly = true)
 public class RoleService {
-  private RolePersistenceAdapter rolePersistenceAdapter;
+  private RoleRepositoryInterface roleRepositoryInterface;
 
-  public RoleService(RolePersistenceAdapter rolePersistenceAdapter) {
-    this.rolePersistenceAdapter = rolePersistenceAdapter;
+  public RoleService(RoleRepositoryInterface roleRepositoryInterface) {
+    this.roleRepositoryInterface = roleRepositoryInterface;
   }
 
   public Role getRoleById(int id) {
-    Optional<Role> optionalRole = rolePersistenceAdapter.findById(id);
+    Optional<Role> optionalRole = roleRepositoryInterface.findById(id);
     Role role =
         optionalRole.orElseThrow(
             () -> new ResourceNotFoundException("Role not found in database using the id: " + id));
@@ -32,12 +28,6 @@ public class RoleService {
 
   @Transactional
   public void saveRole(Role role) {
-    try {
-      rolePersistenceAdapter.save(role);
-    } catch (DataIntegrityViolationException exception) {
-      throw new RoleAlreadyExistsException("Role Name already in use", exception);
-    } catch (DataAccessResourceFailureException exception) {
-      throw new SystemUnavailableException("Persistence service is down", exception);
-    }
+    roleRepositoryInterface.save(role);
   }
 }

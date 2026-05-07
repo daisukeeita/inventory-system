9b67cb86b07edbba8ae317016c922ce4eabdc188
javv <vinas.johnandrei@gmail.com>
2026-05-07 21:26:59 +0800

    configured @Transactional annotation and catching database exception

--- src/main/java/com/javv/inventorySystem/application/service/RoleService.java
+++ src/main/java/com/javv/inventorySystem/application/service/RoleService.java
@@ -2,15 +2,19 @@ package com.javv.inventorySystem.application.service;
 
 import java.util.Optional;
 
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.javv.inventorySystem.domain.exception.ResourceNotFoundException;
+import com.javv.inventorySystem.domain.exception.RoleAlreadyExistsException;
+import com.javv.inventorySystem.domain.exception.SystemUnavailableException;
 import com.javv.inventorySystem.domain.model.role.Role;
 import com.javv.inventorySystem.infrastructure.persistence.role.RolePersistenceAdapter;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class RoleService {
   private RolePersistenceAdapter rolePersistenceAdapter;
 
@@ -20,13 +24,20 @@ public class RoleService {
 
   public Role getRoleById(int id) {
     Optional<Role> optionalRole = rolePersistenceAdapter.findById(id);
-    Role role = optionalRole
-        .orElseThrow(() -> new ResourceNotFoundException(
-            "Role not found in database base using the id: " + id));
+    Role role =
+        optionalRole.orElseThrow(
+            () -> new ResourceNotFoundException("Role not found in database using the id: " + id));
     return role;
   }
 
+  @Transactional
   public void saveRole(Role role) {
-    rolePersistenceAdapter.save(role);
+    try {
+      rolePersistenceAdapter.save(role);
+    } catch (DataIntegrityViolationException exception) {
+      throw new RoleAlreadyExistsException("Role Name already in use", exception);
+    } catch (DataAccessResourceFailureException exception) {
+      throw new SystemUnavailableException("Persistence service is down", exception);
+    }
   }
 }

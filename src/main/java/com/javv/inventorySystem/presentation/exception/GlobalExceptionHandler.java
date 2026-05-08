2caf0b07ace90a21783ae820b4f4d29718130847
javv <vinas.johnandrei@gmail.com>
2026-05-08 12:33:46 +0800

    renamed exception handler for existing entity

--- src/main/java/com/javv/inventorySystem/presentation/exception/GlobalExceptionHandler.java
+++ src/main/java/com/javv/inventorySystem/presentation/exception/GlobalExceptionHandler.java
@@ -1,12 +1,13 @@
 package com.javv.inventorySystem.presentation.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.javv.inventorySystem.domain.exception.EntityAlreadyExistsException;
 import com.javv.inventorySystem.domain.exception.ResourceNotFoundException;
-import com.javv.inventorySystem.domain.exception.RoleAlreadyExistsException;
 import com.javv.inventorySystem.domain.exception.SystemUnavailableException;
 import com.javv.inventorySystem.presentation.payload.ApiResponse;
 
@@ -17,18 +18,29 @@ public class GlobalExceptionHandler {
   public ResponseEntity<ApiResponse<Void>> handleResourceNotFoundException(
       ResourceNotFoundException exception) {
 
-    ApiResponse<Void> response =
-        ApiResponse.error(exception.getMessage(), HttpStatus.NOT_FOUND.value());
+    ApiResponse<Void> response = ApiResponse.error(
+        exception.getMessage(), HttpStatus.NOT_FOUND.value());
 
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
   }
 
-  @ExceptionHandler(RoleAlreadyExistsException.class)
-  public ResponseEntity<ApiResponse<Void>> handleRoleAlreadyExistsException(
-      RoleAlreadyExistsException exception) {
+  @ExceptionHandler(EntityAlreadyExistsException.class)
+  public ResponseEntity<ApiResponse<Void>> handleEntityAlreadyExistsException(
+      EntityAlreadyExistsException exception) {
 
-    ApiResponse<Void> response =
-        ApiResponse.error(exception.getMessage(), HttpStatus.CONFLICT.value());
+    ApiResponse<Void> response = ApiResponse.error(
+        exception.getMessage(), HttpStatus.CONFLICT.value());
+
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+  }
+
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  public ResponseEntity<ApiResponse<Void>> handleDataConflict(
+      DataIntegrityViolationException exception) {
+
+    ApiResponse<Void> response = ApiResponse.error(
+        "This Record was recently created by someone else.",
+        HttpStatus.CONFLICT.value());
 
     return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
   }
@@ -37,8 +49,8 @@ public class GlobalExceptionHandler {
   public ResponseEntity<ApiResponse<Void>> handleSystemUnavailableException(
       SystemUnavailableException exception) {
 
-    ApiResponse<Void> response =
-        ApiResponse.error(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+    ApiResponse<Void> response = ApiResponse.error(
+        exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
 
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
   }
@@ -46,8 +58,8 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ApiResponse<Void>> handleGeneralException(Exception exception) {
 
-    ApiResponse<Void> response =
-        ApiResponse.error("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR.value());
+    ApiResponse<Void> response = ApiResponse.error(
+        "Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR.value());
 
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(response);
   }

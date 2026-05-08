57b14827795f6a137b9df197de740de65c7e2f5a
javv <vinas.johnandrei@gmail.com>
2026-05-08 12:31:22 +0800

    added method for entity retrieval

--- src/main/java/com/javv/inventorySystem/application/service/RoleService.java
+++ src/main/java/com/javv/inventorySystem/application/service/RoleService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.javv.inventorySystem.domain.exception.EntityAlreadyExistsException;
 import com.javv.inventorySystem.domain.exception.ResourceNotFoundException;
 import com.javv.inventorySystem.domain.model.role.Role;
 import com.javv.inventorySystem.domain.repository.RoleRepositoryInterface;
@@ -20,14 +21,23 @@ public class RoleService {
 
   public Role getRoleById(int id) {
     Optional<Role> optionalRole = roleRepositoryInterface.findById(id);
-    Role role =
-        optionalRole.orElseThrow(
-            () -> new ResourceNotFoundException("Role not found in database using the id: " + id));
+    Role role = optionalRole.orElseThrow(
+        () -> new ResourceNotFoundException("Role not found in database using the id: " + id));
+    return role;
+  }
+
+  public Role getRoleByName(String name) {
+    Optional<Role> optionalRole = roleRepositoryInterface.findByName(name.trim().toUpperCase());
+    Role role = optionalRole.orElseThrow(
+        () -> new ResourceNotFoundException("Role not found in database using the name: " + name));
     return role;
   }
 
   @Transactional
   public void saveRole(Role role) {
+    if (roleRepositoryInterface.existsByName(role.getName())) {
+      throw new EntityAlreadyExistsException("Role Name already in use!");
+    }
     roleRepositoryInterface.save(role);
   }
 }

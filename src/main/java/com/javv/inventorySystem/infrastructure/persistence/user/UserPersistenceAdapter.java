9f800c1ef6671f0b6c4a2bf1ab7c5bfa6341b9d1
javv <vinas.johnandrei@gmail.com>
2026-05-10 21:12:45 +0800

    renamed injected dependency

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/user/UserPersistenceAdapter.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/user/UserPersistenceAdapter.java
@@ -10,42 +10,41 @@ import com.javv.inventorySystem.domain.model.user.User;
 import com.javv.inventorySystem.domain.repository.UserRepositoryInterface;
 
 /**
- * This is where the system "translates" the JPA Entity (User) to Domain Entity
- * (User) after getting
+ * This is where the system "translates" the JPA Entity (User) to Domain Entity (User) after getting
  * the data from the database.
  *
- * <p>
- * This is where the system "translates" the Domain Entity (User) to JPA Entity
- * (User) before
+ * <p>This is where the system "translates" the Domain Entity (User) to JPA Entity (User) before
  * inserting the object to the database.
  */
 @Repository
 public class UserPersistenceAdapter implements UserRepositoryInterface {
   private final UserJpaRepository userJpaRepository;
-  private final UserPersistenceMapper userMapper;
+  private final UserPersistenceMapper userPersistenceMapper;
 
   public UserPersistenceAdapter(
       UserJpaRepository userJpaRepository, UserPersistenceMapper userPersistenceMapper) {
     this.userJpaRepository = userJpaRepository;
-    this.userMapper = userPersistenceMapper;
+    this.userPersistenceMapper = userPersistenceMapper;
   }
 
   @Override
   public Optional<User> findByUsername(String username) {
     Optional<UserJpaEntity> jpaEntity = userJpaRepository.findByUsername(username);
-    Optional<User> user = jpaEntity.map(entity -> userMapper.jpaToDomainEntity(entity));
+    Optional<User> user = jpaEntity.map(entity -> userPersistenceMapper.toDomainEntity(entity));
 
     return user;
   }
 
   @Override
-  public void save(User user) {
-    UserJpaEntity jpaEntity = userMapper.domainToJpaEntity(
-        user.getRole(), user.getPersonalDetails(), user.getContactInformation(), user);
+  public User save(User user) {
+    UserJpaEntity jpaEntity = userPersistenceMapper.toJpaEntity(user);
+
     try {
       userJpaRepository.save(jpaEntity);
     } catch (DataAccessResourceFailureException exception) {
       throw new SystemUnavailableException("Persistence service is down", exception);
     }
+
+    return userPersistenceMapper.toDomainEntity(jpaEntity);
   }
 }

085852cac50822fd75b32107488a591e78e63ef3
javv <vinas.johnandrei@gmail.com>
2026-05-10 21:12:56 +0800

    controller layer for user entity

---
+++ src/main/java/com/javv/inventorySystem/presentation/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.javv.inventorySystem.presentation.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javv.inventorySystem.application.dto.user.UserCreateDto;
+import com.javv.inventorySystem.application.service.UserService;
+import com.javv.inventorySystem.domain.model.user.User;
+import com.javv.inventorySystem.presentation.payload.ApiResponse;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+public class UserController {
+  private UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @PostMapping("/save")
+  @ResponseStatus(HttpStatus.CREATED)
+  public ApiResponse<User> saveUser(@RequestBody UserCreateDto userCreateDto) {
+    User user = userService.saveUser(userCreateDto);
+    return ApiResponse.success(user, "Successfully created new user.", HttpStatus.CREATED.value());
+  }
+}

e8bc8366802a6797f3089f09735cffd1f4afd3a2
javv <vinas.johnandrei@gmail.com>
2026-05-12 02:26:04 +0800

    added entry point for user retrieval

--- src/main/java/com/javv/inventorySystem/presentation/controller/UserController.java
+++ src/main/java/com/javv/inventorySystem/presentation/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.javv.inventorySystem.presentation.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,14 @@ public class UserController {
     UserResponseDto userResponseDto = userService.saveUser(userCreateDto);
     return ApiResponse.success(
         userResponseDto, "Successfully created new user.", HttpStatus.CREATED.value());
+  }
+
+  @GetMapping("/getByUsername/{username}")
+  @ResponseStatus(HttpStatus.FOUND)
+  public ApiResponse<UserResponseDto> getByUsername(@PathVariable String username) {
+    UserResponseDto userResponseDto = userService.getUserByUsername(username);
+
+    return ApiResponse.success(
+        userResponseDto, "Successfully found the user", HttpStatus.FOUND.value());
   }
 }

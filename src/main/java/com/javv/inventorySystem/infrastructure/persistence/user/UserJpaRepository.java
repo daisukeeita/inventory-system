b0dd424554697557241b29b852ce599550de3617
javv <vinas.johnandrei@gmail.com>
2026-05-11 13:22:18 +0800

    added save method with UserJpaEntity returned type

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/user/UserJpaRepository.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/user/UserJpaRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserJpaRepository extends JpaRepository<UserJpaEntity, UUID> {
 
+  UserJpaEntity save(UserJpaEntity userJpaEntity);
+
   Optional<UserJpaEntity> findByUsername(String username);
 }

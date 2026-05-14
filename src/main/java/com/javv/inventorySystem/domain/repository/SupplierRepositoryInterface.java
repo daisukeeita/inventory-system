dc25a56d3c3a5c2b81d616f5caabf41841114457
javv <vinas.johnandrei@gmail.com>
2026-05-14 13:03:03 +0800

    init supplier repository interface

---
+++ src/main/java/com/javv/inventorySystem/domain/repository/SupplierRepositoryInterface.java
@@ -0,0 +1,11 @@
+package com.javv.inventorySystem.domain.repository;
+
+import java.util.Optional;
+
+import com.javv.inventorySystem.domain.model.supplier.Supplier;
+
+public interface SupplierRepositoryInterface {
+  Supplier save(Supplier supplier);
+
+  Optional<Supplier> findByName(String companyName);
+}

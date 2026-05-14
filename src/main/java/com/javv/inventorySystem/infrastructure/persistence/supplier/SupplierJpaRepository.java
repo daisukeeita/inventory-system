812b163dbc470e16298fc689cf54b14ae30a955f
javv <vinas.johnandrei@gmail.com>
2026-05-14 13:03:29 +0800

    init supplier jpa repository

---
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/supplier/SupplierJpaRepository.java
@@ -0,0 +1,9 @@
+package com.javv.inventorySystem.infrastructure.persistence.supplier;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierJpaRepository extends JpaRepository<SupplierJpaEntity, Integer> {
+  Optional<SupplierJpaEntity> findByCompanyName(String companyName);
+}

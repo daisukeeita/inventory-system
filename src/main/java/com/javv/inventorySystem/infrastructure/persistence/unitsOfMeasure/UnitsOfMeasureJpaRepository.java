59629df3675465e4dd7824fa3a2fd08df728b2b6
javv <vinas.johnandrei@gmail.com>
2026-05-16 03:01:29 +0800

    init

---
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/unitsOfMeasure/UnitsOfMeasureJpaRepository.java
@@ -0,0 +1,11 @@
+package com.javv.inventorySystem.infrastructure.persistence.unitsOfMeasure;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UnitsOfMeasureJpaRepository
+    extends JpaRepository<UnitsOfMeasureJpaEntity, Integer> {
+
+  Optional<UnitsOfMeasureJpaEntity> findByName(String name);
+}

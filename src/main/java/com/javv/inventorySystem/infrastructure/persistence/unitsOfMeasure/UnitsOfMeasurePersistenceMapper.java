4cf1771a0aed8226a871dbbe56f76bdd7258f5f0
javv <vinas.johnandrei@gmail.com>
2026-05-16 03:01:34 +0800

    init

---
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/unitsOfMeasure/UnitsOfMeasurePersistenceMapper.java
@@ -0,0 +1,26 @@
+package com.javv.inventorySystem.infrastructure.persistence.unitsOfMeasure;
+
+import org.springframework.stereotype.Component;
+
+import com.javv.inventorySystem.domain.model.product.UnitsOfMeasure;
+
+@Component
+public class UnitsOfMeasurePersistenceMapper {
+
+  public UnitsOfMeasureJpaEntity toJpaEntity(UnitsOfMeasure unitsOfMeasure) {
+    UnitsOfMeasureJpaEntity unitsOfMeasureJpaEntity = new UnitsOfMeasureJpaEntity();
+    unitsOfMeasureJpaEntity.setName(unitsOfMeasure.getName());
+    unitsOfMeasureJpaEntity.setAbbreviation(unitsOfMeasure.getAbbreviation());
+
+    return unitsOfMeasureJpaEntity;
+  }
+
+  public UnitsOfMeasure toDomainEntity(UnitsOfMeasureJpaEntity unitsOfMeasureJpaEntity) {
+    UnitsOfMeasure unitsOfMeasure = new UnitsOfMeasure();
+    unitsOfMeasure.setId(unitsOfMeasureJpaEntity.getId());
+    unitsOfMeasure.setName(unitsOfMeasure.getName());
+    unitsOfMeasure.setAbbreviation(unitsOfMeasure.getAbbreviation());
+
+    return unitsOfMeasure;
+  }
+}

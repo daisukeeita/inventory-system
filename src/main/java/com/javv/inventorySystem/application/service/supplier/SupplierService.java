9b9fcbc1db1d19959bcf354c55ccf274387e6a66
javv <vinas.johnandrei@gmail.com>
2026-05-15 13:30:20 +0800

    updated supplier service methods

--- src/main/java/com/javv/inventorySystem/application/service/supplier/SupplierService.java
+++ src/main/java/com/javv/inventorySystem/application/service/supplier/SupplierService.java
@@ -1,8 +1,14 @@
 package com.javv.inventorySystem.application.service.supplier;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.javv.inventorySystem.application.command.supplier.SupplierRegisterCommand;
+import com.javv.inventorySystem.domain.exception.ResourceNotFoundException;
+import com.javv.inventorySystem.domain.model.supplier.Supplier;
+import com.javv.inventorySystem.domain.model.supplier.SupplierAddress;
 import com.javv.inventorySystem.domain.repository.SupplierRepositoryInterface;
 
 @Service
@@ -13,5 +19,51 @@ public class SupplierService {
 
   public SupplierService(SupplierRepositoryInterface supplierRepositoryInterface) {
     this.supplierRepositoryInterface = supplierRepositoryInterface;
+  }
+
+  @Transactional
+  public Supplier saveSupplier(SupplierRegisterCommand supplierRegisterCommand) {
+    Supplier supplier = toDomainEntity(supplierRegisterCommand);
+
+    return supplierRepositoryInterface.save(supplier);
+  }
+
+  public Supplier findByName(String companyName) {
+    Optional<Supplier> optionalSupplier = supplierRepositoryInterface.findByName(companyName);
+
+    Supplier supplier = optionalSupplier.orElseThrow(
+        () -> new ResourceNotFoundException(
+            "Supplier was not found using companyName: ''" + companyName + "'"));
+
+    return supplier;
+  }
+
+  public Supplier findById(int id) {
+    Optional<Supplier> optionalSupplier = supplierRepositoryInterface.findById(id);
+
+    Supplier supplier = optionalSupplier.orElseThrow(
+        () -> new ResourceNotFoundException(
+            "Supplier was not found using ID: ''" + id + "'"));
+
+    return supplier;
+
+  }
+
+  private Supplier toDomainEntity(SupplierRegisterCommand supplierRegisterCommand) {
+    SupplierAddress supplierAddress = new SupplierAddress();
+    supplierAddress.setStreet(supplierRegisterCommand.street());
+    supplierAddress.setCity(supplierRegisterCommand.city());
+    supplierAddress.setState(supplierRegisterCommand.state());
+    supplierAddress.setPostalCode(supplierRegisterCommand.postalCode());
+    supplierAddress.setCountry(supplierRegisterCommand.country());
+
+    Supplier supplier = new Supplier();
+    supplier.setCompanyName(supplierRegisterCommand.companyName());
+    supplier.setContactName(supplierRegisterCommand.contractName());
+    supplier.setPhoneNumber(supplierRegisterCommand.phoneNumber());
+    supplier.setEmail(supplierRegisterCommand.email());
+    supplier.setSupplierAddress(supplierAddress);
+
+    return supplier;
   }
 }

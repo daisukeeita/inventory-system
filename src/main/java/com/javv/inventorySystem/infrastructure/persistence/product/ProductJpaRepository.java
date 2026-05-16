6cad9ddf18547a0c4577e4e940a04465cc98bf6d
javv <vinas.johnandrei@gmail.com>
2026-05-17 05:14:53 +0800

    updated format

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/product/ProductJpaRepository.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/product/ProductJpaRepository.java
@@ -2,5 +2,4 @@ package com.javv.inventorySystem.infrastructure.persistence.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductJpaRepository extends JpaRepository<ProductJpaEntity, String> {
-}
+public interface ProductJpaRepository extends JpaRepository<ProductJpaEntity, String> {}

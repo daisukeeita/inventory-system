1c49ba1e44c261d189d8b0262a25946fbbbcdf0b
javv <vinas.johnandrei@gmail.com>
2026-05-17 05:15:21 +0800

    updated imported files

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/unitsOfMeasure/UnitsOfMeasureJpaEntity.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/unitsOfMeasure/UnitsOfMeasureJpaEntity.java
@@ -1,7 +1,14 @@
-package com.javv.inventorySystem.infrastructure.persistence.product;
+package com.javv.inventorySystem.infrastructure.persistence.unitsOfMeasure;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -9,17 +16,26 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "units_of_measure")
+@EntityListeners(AuditingEntityListener.class)
 public class UnitsOfMeasureJpaEntity {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE)
-  private int id;
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
 
   @Column(name = "name", nullable = false, length = 20, unique = true)
   private String name;
 
   @Column(name = "abbreviation", nullable = true, length = 10)
   private String abbreviation;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private Instant createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private Instant updatedAt;
 
   public UnitsOfMeasureJpaEntity() {}
 
@@ -28,7 +44,7 @@ public class UnitsOfMeasureJpaEntity {
     this.abbreviation = abbreviation;
   }
 
-  public void setId(int id) {
+  public void setId(Integer id) {
     this.id = id;
   }
 
@@ -40,7 +56,15 @@ public class UnitsOfMeasureJpaEntity {
     this.abbreviation = abbreviation;
   }
 
-  public int getId() {
+  protected void setCreatedAt(Instant createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  protected void setUpdatedAt(Instant updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public Integer getId() {
     return id;
   }
 
@@ -50,5 +74,13 @@ public class UnitsOfMeasureJpaEntity {
 
   public String getAbbreviation() {
     return abbreviation;
+  }
+
+  public Instant getCreatedAt() {
+    return createdAt;
+  }
+
+  public Instant getUpdatedAt() {
+    return updatedAt;
   }
 }

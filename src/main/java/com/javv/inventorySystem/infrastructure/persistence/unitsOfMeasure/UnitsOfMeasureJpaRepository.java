012783b291555b32c078865f246e969e5b3be104
javv <vinas.johnandrei@gmail.com>
2026-05-17 05:15:30 +0800

    added Repository annotation

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/unitsOfMeasure/UnitsOfMeasureJpaRepository.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/unitsOfMeasure/UnitsOfMeasureJpaRepository.java
@@ -3,7 +3,9 @@ package com.javv.inventorySystem.infrastructure.persistence.unitsOfMeasure;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UnitsOfMeasureJpaRepository
     extends JpaRepository<UnitsOfMeasureJpaEntity, Integer> {
 

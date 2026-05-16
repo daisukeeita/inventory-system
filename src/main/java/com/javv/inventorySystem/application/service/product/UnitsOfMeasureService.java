ab00582821c9f1efb8eabae9b233bd6668e7256f
javv <vinas.johnandrei@gmail.com>
2026-05-17 05:14:00 +0800

    updated Transactional property

--- src/main/java/com/javv/inventorySystem/application/service/product/UnitsOfMeasureService.java
+++ src/main/java/com/javv/inventorySystem/application/service/product/UnitsOfMeasureService.java
@@ -2,16 +2,18 @@ package com.javv.inventorySystem.application.service.product;
 
 import java.util.Optional;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.javv.inventorySystem.application.command.product.UnitsOfMeasureCommand;
+import com.javv.inventorySystem.domain.exception.EntityAlreadyExistsException;
 import com.javv.inventorySystem.domain.exception.ResourceNotFoundException;
 import com.javv.inventorySystem.domain.model.product.UnitsOfMeasure;
 import com.javv.inventorySystem.domain.repository.UnitsOfMeasureRepositoryInterface;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class UnitsOfMeasureService {
 
   private UnitsOfMeasureRepositoryInterface unitsOfMeasureRepositoryInterface;
@@ -25,9 +27,29 @@ public class UnitsOfMeasureService {
   public UnitsOfMeasure saveMeasure(UnitsOfMeasureCommand unitsOfMeasureCommand) {
     UnitsOfMeasure unitsOfMeasure = toDomainEntity(unitsOfMeasureCommand);
 
-    UnitsOfMeasure savedUnitsOfMeasure = unitsOfMeasureRepositoryInterface.save(unitsOfMeasure);
+    try {
+      UnitsOfMeasure savedUnitsOfMeasure = unitsOfMeasureRepositoryInterface.save(unitsOfMeasure);
+      return savedUnitsOfMeasure;
 
-    return savedUnitsOfMeasure;
+    } catch (DataIntegrityViolationException exception) {
+      throw new EntityAlreadyExistsException(
+          "This unit of measure is already registered.", exception);
+    }
+  }
+
+  @Transactional
+  public UnitsOfMeasure updateMeasure(Integer id, UnitsOfMeasureCommand unitsOfMeasureCommand) {
+    Optional<UnitsOfMeasure> optionalMeasure = unitsOfMeasureRepositoryInterface.getById(id);
+
+    UnitsOfMeasure unitsOfMeasure =
+        optionalMeasure.orElseThrow(
+            () -> new ResourceNotFoundException("Units of Measure not found by id: ;" + id + "'"));
+    unitsOfMeasure.setName(unitsOfMeasureCommand.name());
+    unitsOfMeasure.setAbbreviation(unitsOfMeasureCommand.abbreviation());
+
+    UnitsOfMeasure savedMeasure = unitsOfMeasureRepositoryInterface.save(unitsOfMeasure);
+
+    return savedMeasure;
   }
 
   public UnitsOfMeasure getById(int id) {

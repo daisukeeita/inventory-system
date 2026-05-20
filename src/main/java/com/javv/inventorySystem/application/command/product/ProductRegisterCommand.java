24310ac311c3a437d39c18d58fca1a0f4bc8fb78
javv <vinas.johnandrei@gmail.com>
2026-05-20 12:08:59 +0800

    added checker

--- src/main/java/com/javv/inventorySystem/application/command/product/ProductRegisterCommand.java
+++ src/main/java/com/javv/inventorySystem/application/command/product/ProductRegisterCommand.java
@@ -1,8 +1,25 @@
 package com.javv.inventorySystem.application.command.product;
 
+import java.util.Objects;
+
 public record ProductRegisterCommand(
     String sku,
     String name,
     String supplier,
     String baseUnitOfMeasure) {
+
+  public ProductRegisterCommand {
+
+    Objects.requireNonNull(sku,
+        "Product Register Command: SKU cannot be null");
+
+    Objects.requireNonNull(name,
+        "Product Register Command: Product Name cannot be null.");
+
+    Objects.requireNonNull(supplier,
+        "Product Register Command: Supplier Name cannot be null.");
+
+    Objects.requireNonNull(baseUnitOfMeasure,
+        "Product Register Command: Base Unit of Measure cannot be null.");
+  }
 }

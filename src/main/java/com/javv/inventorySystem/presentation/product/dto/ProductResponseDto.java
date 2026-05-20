265f4c55c1b19c3634299eb090bdc1ba3fde6dcd
javv <vinas.johnandrei@gmail.com>
2026-05-20 12:11:52 +0800

    added checker

--- src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductResponseDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductResponseDto.java
@@ -1,4 +1,13 @@
 package com.javv.inventorySystem.presentation.product.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ProductResponseDto(
-    String sku, String name, String supplier, String baseUnitOfMeasure) {}
+    @NotBlank(message = "Product Response DTO: SKU is required for response.") String sku,
+
+    @NotBlank(message = "Product Response DTO: Product Name is required for response.") String name,
+
+    @NotBlank(message = "Product Response DTO: Supplier Name is required for response.") String supplier,
+
+    @NotBlank(message = "Product Response DTO: Base Unit of Measure is required for response.") String baseUnitOfMeasure) {
+}

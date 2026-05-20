71ca0f6a0da1d1d1d6384281a12e93494cbc38af
javv <vinas.johnandrei@gmail.com>
2026-05-20 12:13:15 +0800

    rename class name and method name

--- src/main/java/com/javv/inventorySystem/presentation/supplier/SupplierDtoMapper.java
+++ src/main/java/com/javv/inventorySystem/presentation/supplier/SupplierDtoMapper.java
@@ -1,43 +1,59 @@
 package com.javv.inventorySystem.presentation.supplier;
 
+import java.util.Objects;
+
 import org.springframework.stereotype.Component;
 
 import com.javv.inventorySystem.application.command.supplier.SupplierRegisterCommand;
-import com.javv.inventorySystem.application.command.supplier.SupplierUpdateAddressCommand;
+import com.javv.inventorySystem.application.command.supplier.SupplierUpdateCommand;
 import com.javv.inventorySystem.domain.model.supplier.Supplier;
-import com.javv.inventorySystem.presentation.supplier.dto.SupplierRegistrationDto;
+import com.javv.inventorySystem.presentation.supplier.dto.SupplierRegisterDto;
 import com.javv.inventorySystem.presentation.supplier.dto.SupplierResponseDto;
-import com.javv.inventorySystem.presentation.supplier.dto.SupplierUpdateAddressDto;
+import com.javv.inventorySystem.presentation.supplier.dto.SupplierUpdateDto;
 
 @Component
 public class SupplierDtoMapper {
 
-  public SupplierRegisterCommand toCommandRecord(SupplierRegistrationDto supplierRegistrationDto) {
+  public SupplierRegisterCommand toRegisterCommandRecord(SupplierRegisterDto supplierRegisterDto) {
+
+    Objects.requireNonNull(supplierRegisterDto,
+        "Supplier DTO Mapper: Cannot map a null SupplierRegisterDto to a Command Record.");
 
     return new SupplierRegisterCommand(
-        supplierRegistrationDto.companyName(),
-        supplierRegistrationDto.contactName(),
-        supplierRegistrationDto.phoneNumber(),
-        supplierRegistrationDto.email(),
-        supplierRegistrationDto.street(),
-        supplierRegistrationDto.city(),
-        supplierRegistrationDto.state(),
-        supplierRegistrationDto.postalCode(),
-        supplierRegistrationDto.country());
+        supplierRegisterDto.companyName(),
+        supplierRegisterDto.contactName(),
+        supplierRegisterDto.phoneNumber(),
+        supplierRegisterDto.email(),
+        supplierRegisterDto.street(),
+        supplierRegisterDto.city(),
+        supplierRegisterDto.state(),
+        supplierRegisterDto.postalCode(),
+        supplierRegisterDto.country());
   }
 
-  public SupplierUpdateAddressCommand toAddressUpdateCommandRecord(
-      SupplierUpdateAddressDto supplierUpdateAddressDto) {
+  public SupplierUpdateCommand toUpdateCommandRecord(
+      SupplierUpdateDto supplierUpdateDto) {
 
-    return new SupplierUpdateAddressCommand(
-        supplierUpdateAddressDto.street(),
-        supplierUpdateAddressDto.city(),
-        supplierUpdateAddressDto.state(),
-        supplierUpdateAddressDto.postalCode(),
-        supplierUpdateAddressDto.country());
+    Objects.requireNonNull(supplierUpdateDto,
+        "Supplier DTO Mapper: Cannot map a null SupplierUpdateAddressDto to a Command Record.");
+
+    return new SupplierUpdateCommand(
+        supplierUpdateDto.companyName(),
+        supplierUpdateDto.contactName(),
+        supplierUpdateDto.phoneNumber(),
+        supplierUpdateDto.email(),
+        supplierUpdateDto.street(),
+        supplierUpdateDto.city(),
+        supplierUpdateDto.state(),
+        supplierUpdateDto.postalCode(),
+        supplierUpdateDto.country());
   }
 
   public SupplierResponseDto toResponseDto(Supplier supplier) {
+
+    Objects.requireNonNull(supplier,
+        "Supplier DTO Mapper: Cannot map a null Supplier to a Response DTO.");
+
     String address = supplier.getSupplierAddress().getStreet()
         + ", "
         + supplier.getSupplierAddress().getCity()

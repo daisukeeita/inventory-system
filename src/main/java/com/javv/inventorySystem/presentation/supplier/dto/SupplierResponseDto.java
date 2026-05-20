cf99a455c1227f3a45b6aae90f178eca38ccaac2
javv <vinas.johnandrei@gmail.com>
2026-05-20 12:12:40 +0800

    added checker

--- src/main/java/com/javv/inventorySystem/presentation/supplier/dto/SupplierResponseDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/supplier/dto/SupplierResponseDto.java
@@ -1,9 +1,15 @@
 package com.javv.inventorySystem.presentation.supplier.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record SupplierResponseDto(
-    String companyName,
-    String contactName,
-    String phoneNumber,
-    String email,
-    String address) {
+    @NotBlank(message = "Supplier Response DTO: Company Name is required.") String companyName,
+
+    @NotBlank(message = "Supplier Response DTO: Contact Name is required.") String contactName,
+
+    @NotBlank(message = "Supplier Response DTO: Phone Number is required.") String phoneNumber,
+
+    @NotBlank(message = "Supplier Response DTO: Email is required.") String email,
+
+    @NotBlank(message = "Supplier Response DTO: Address is required.") String address) {
 }

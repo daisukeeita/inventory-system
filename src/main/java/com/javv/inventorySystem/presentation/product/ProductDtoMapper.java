70c1efcd978a6ac8b7072428309a36d0ac3b2850
javv <vinas.johnandrei@gmail.com>
2026-05-21 12:54:45 +0800

    added mapper method

--- src/main/java/com/javv/inventorySystem/presentation/product/ProductDtoMapper.java
+++ src/main/java/com/javv/inventorySystem/presentation/product/ProductDtoMapper.java
@@ -5,9 +5,11 @@ import java.util.Objects;
 import org.springframework.stereotype.Component;
 
 import com.javv.inventorySystem.application.command.product.ProductRegisterCommand;
+import com.javv.inventorySystem.application.command.product.ProductUpdateCommand;
 import com.javv.inventorySystem.domain.model.product.Product;
 import com.javv.inventorySystem.presentation.product.dto.ProductRegisterDto;
 import com.javv.inventorySystem.presentation.product.dto.ProductResponseDto;
+import com.javv.inventorySystem.presentation.product.dto.ProductUpdateDto;
 
 @Component
 public class ProductDtoMapper {
@@ -24,12 +26,29 @@ public class ProductDtoMapper {
         productRegisterDto.baseUnitOfMeasure().trim());
   }
 
+  public ProductUpdateCommand toUpdateCommand(ProductUpdateDto productUpdateDto) {
+
+    Objects.requireNonNull(productUpdateDto,
+        "Product DTO Mapper: Cannot map a null ProductUpdateDto to a Command Record.");
+
+    return new ProductUpdateCommand(
+        productUpdateDto.sku(),
+        productUpdateDto.name(),
+        productUpdateDto.supplier(),
+        productUpdateDto.baseUnitOfMeasure());
+  }
+
   public ProductResponseDto toResponseDto(Product product) {
 
+    Objects.requireNonNull(product,
+        "Product DTO Mapper: Cannot map a null Product to a Response DTO Record.");
+
     return new ProductResponseDto(
+        product.getId(),
         product.getSku().trim(),
         product.getName().trim(),
         product.getSupplier().getCompanyName().trim(),
         product.getBaseUnitsOfMeasure().getName().trim());
   }
+
 }

f352461ccf62f3d3c6e01a325d412b294c7348be
javv <vinas.johnandrei@gmail.com>
2026-05-21 12:53:35 +0800

    added methods

--- src/main/java/com/javv/inventorySystem/domain/repository/ProductRepositoryInterface.java
+++ src/main/java/com/javv/inventorySystem/domain/repository/ProductRepositoryInterface.java
@@ -7,5 +7,9 @@ import com.javv.inventorySystem.domain.model.product.Product;
 public interface ProductRepositoryInterface {
   Product save(Product product);
 
+  Product update(Product product);
+
   Optional<Product> getBySku(String sku);
+
+  Optional<Product> getById(Long id);
 }

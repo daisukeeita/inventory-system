303b029e98411b464fda8e50a867e73529e4b60e
javv <vinas.johnandrei@gmail.com>
2026-05-21 23:11:09 +0800

    Repository Interface for Product Packaging

---
+++ src/main/java/com/javv/inventorySystem/domain/repository/ProductPackagingRepositoryInterface.java
@@ -0,0 +1,16 @@
+package com.javv.inventorySystem.domain.repository;
+
+import java.util.Optional;
+
+import com.javv.inventorySystem.domain.model.product.ProductPackaging;
+
+public interface ProductPackagingRepositoryInterface {
+
+  ProductPackaging save(ProductPackaging productPackaging);
+
+  ProductPackaging updated(ProductPackaging productPackaging);
+
+  Optional<ProductPackaging> getByPackagingCode(String packagingCode);
+
+  Optional<ProductPackaging> getById(Integer id);
+}

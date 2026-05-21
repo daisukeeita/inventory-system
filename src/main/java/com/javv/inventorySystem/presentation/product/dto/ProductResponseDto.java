6bc5dd02c59717f300683df27b789455e28e904c
javv <vinas.johnandrei@gmail.com>
2026-05-21 12:54:17 +0800

    added id for response

--- src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductResponseDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductResponseDto.java
@@ -1,8 +1,11 @@
 package com.javv.inventorySystem.presentation.product.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ProductResponseDto(
+    @NotNull(message = "Product Response DTO: ID is required for response.") Long id,
+
     @NotBlank(message = "Product Response DTO: SKU is required for response.") String sku,
 
     @NotBlank(message = "Product Response DTO: Product Name is required for response.") String name,

f8d88817dfb19101b58c521ecd5eac2cf6166e8a
javv <vinas.johnandrei@gmail.com>
2026-05-26 13:23:42 +0800

    updated query to find product sku from Main Inventory Record

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/mainInventory/MainInventoryJpaRepository.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/mainInventory/MainInventoryJpaRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MainInventoryJpaRepository extends JpaRepository<MainInventoryJpaEntity, Integer> {
 
-  Optional<MainInventoryJpaEntity> findBySku(String sku);
+  Optional<MainInventoryJpaEntity> findByProductJpaEntitySku(String sku);
 }

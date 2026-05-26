d978db844e2a259d93a10e8a22c3b013ac81cac5
javv <vinas.johnandrei@gmail.com>
2026-05-26 13:23:03 +0800

    init Main Inventory Service

---
+++ src/main/java/com/javv/inventorySystem/application/service/mainInventory/MainInventoryService.java
@@ -0,0 +1,54 @@
+package com.javv.inventorySystem.application.service.mainInventory;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.javv.inventorySystem.application.command.mainInventory.MainInventoryRegisterCommand;
+import com.javv.inventorySystem.application.service.product.ProductService;
+import com.javv.inventorySystem.domain.exception.EntityAlreadyExistsException;
+import com.javv.inventorySystem.domain.model.mainInventory.MainInventory;
+import com.javv.inventorySystem.domain.model.product.Product;
+import com.javv.inventorySystem.domain.repository.MainInventoryRepositoryInterface;
+
+@Service
+@Transactional(readOnly = true)
+public class MainInventoryService {
+
+  private ProductService productService;
+  private MainInventoryRepositoryInterface mainInventoryRepositoryInterface;
+
+  public MainInventoryService(
+      ProductService productService,
+      MainInventoryRepositoryInterface mainInventoryRepositoryInterface) {
+    this.productService = productService;
+    this.mainInventoryRepositoryInterface = mainInventoryRepositoryInterface;
+  }
+
+  @Transactional
+  public MainInventory saveInventory(MainInventoryRegisterCommand mainInventoryRegisterCommand) {
+    Product product = productService.getBySku(mainInventoryRegisterCommand.sku());
+
+    MainInventory mainInventory = toDomainEntity(mainInventoryRegisterCommand, product);
+
+    try {
+      return mainInventoryRepositoryInterface.save(mainInventory);
+    } catch (DataIntegrityViolationException exception) {
+      throw new EntityAlreadyExistsException(
+          "Main Inventory Persistence Adapter: Inventory for this product already exists.");
+    }
+  }
+
+  private MainInventory toDomainEntity(
+      MainInventoryRegisterCommand mainInventoryRegisterCommand,
+      Product product) {
+
+    MainInventory mainInventory = new MainInventory();
+
+    mainInventory.setProduct(product);
+    mainInventory.setQuantityOnHand(mainInventoryRegisterCommand.quantityOnHand());
+    mainInventory.setReorderLevel(mainInventoryRegisterCommand.reorderLevel());
+
+    return mainInventory;
+  }
+}

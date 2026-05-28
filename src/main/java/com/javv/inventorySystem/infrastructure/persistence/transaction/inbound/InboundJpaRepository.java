b66170fc76415829a347840885d0c7467ae6bf32
javv <vinas.johnandrei@gmail.com>
2026-05-28 21:21:33 +0800

    updated inbound jpa repository

---
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/transaction/inbound/InboundJpaRepository.java
@@ -0,0 +1,11 @@
+package com.javv.inventorySystem.infrastructure.persistence.transaction.inbound;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InboundJpaRepository extends JpaRepository<InboundJpaEntity, Long> {
+
+  Optional<InboundJpaEntity> findByDateReceived(LocalDateTime dateReceived);
+}

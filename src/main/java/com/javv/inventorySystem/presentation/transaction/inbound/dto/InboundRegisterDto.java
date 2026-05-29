8801dea09b80a6334d6f4c643cf12183cd52b11b
javv <vinas.johnandrei@gmail.com>
2026-05-29 13:37:10 +0800

    init Inbound Register DTO

---
+++ src/main/java/com/javv/inventorySystem/presentation/transaction/inbound/dto/InboundRegisterDto.java
@@ -0,0 +1,23 @@
+package com.javv.inventorySystem.presentation.transaction.inbound.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record InboundRegisterDto(
+
+    @NotBlank(message = "Inbound Register DTO: Supplier Name is required for register.") String supplierName,
+
+    @NotBlank(message = "Inbound Register DTO: Encoder Username is required for register.") String username,
+
+    @NotBlank(message = "Inbound Register DTO: Invoice Number is required for register.") String invoiceNumber,
+
+    @NotNull(message = "Inbound Register DTO: Date Received is required for register.") LocalDateTime dateReceived,
+
+    @NotNull(message = "Inbound Register DTO: List Items is required for registration.") List<InboundItemRegisterDto> listInboundItem
+
+) {
+
+}

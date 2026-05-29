eb91ce48436b9c3e7eff937b69ce3c387a0b8564
javv <vinas.johnandrei@gmail.com>
2026-05-29 13:37:04 +0800

    init Inbound Register DTO

---
+++ src/main/java/com/javv/inventorySystem/presentation/transaction/inbound/dto/InboundItemRegisterDto.java
@@ -0,0 +1,16 @@
+package com.javv.inventorySystem.presentation.transaction.inbound.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record InboundItemRegisterDto(
+
+    @NotBlank(message = "Inbound Item Register DTO: Product SKU is required for registration.") String productSku,
+
+    @NotBlank(message = "Inbound Item Register DTO: Packaging Code is required for registration.") String packagingCode,
+
+    @NotNull(message = "Inbound Item Register DTO: Quantity Received is required for registration.") @Min(value = 0, message = "Inbound Item Register DTO: Quantity Received should not be less than zero.") int quantityReceived
+
+) {
+}

8e148f23fa64619daa146b211b9239e5d62ee094
javv <vinas.johnandrei@gmail.com>
2026-05-29 13:16:07 +0800

    init update of persistence mapper

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/transaction/inbound/InboundPersistenceMapper.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/transaction/inbound/InboundPersistenceMapper.java
@@ -3,11 +3,32 @@ package com.javv.inventorySystem.infrastructure.persistence.transaction.inbound;
 import org.springframework.stereotype.Component;
 
 import com.javv.inventorySystem.domain.model.transaction.inbound.Inbound;
+import com.javv.inventorySystem.infrastructure.persistence.product.ProductPersistenceMapper;
+import com.javv.inventorySystem.infrastructure.persistence.productPackaging.ProductPackagingPersistenceMapper;
+import com.javv.inventorySystem.infrastructure.persistence.supplier.SupplierPersistenceMapper;
+import com.javv.inventorySystem.infrastructure.persistence.user.UserPersistenceMapper;
 
 @Component
 public class InboundPersistenceMapper {
 
+  private UserPersistenceMapper userPersistenceMapper;
+  private ProductPersistenceMapper productPersistenceMapper;
+  private SupplierPersistenceMapper supplierPersistenceMapper;
+  private ProductPackagingPersistenceMapper productPackagingPersistenceMapper;
+
+  public InboundPersistenceMapper(
+      UserPersistenceMapper userPersistenceMapper,
+      ProductPersistenceMapper productPersistenceMapper,
+      SupplierPersistenceMapper supplierPersistenceMapper,
+      ProductPackagingPersistenceMapper productPackagingPersistenceMapper) {
+    this.userPersistenceMapper = userPersistenceMapper;
+    this.productPersistenceMapper = productPersistenceMapper;
+    this.supplierPersistenceMapper = supplierPersistenceMapper;
+    this.productPackagingPersistenceMapper = productPackagingPersistenceMapper;
+  }
+
   public InboundJpaEntity toJpaEntity(Inbound inbound) {
+
     return null;
   }
 
@@ -18,4 +39,5 @@ public class InboundPersistenceMapper {
   public InboundJpaEntity updateJpaEntity(Inbound inbound, InboundJpaEntity inboundJpaEntity) {
     return null;
   }
+
 }

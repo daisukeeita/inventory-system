6b1ee61c1dad9064aeb8dcaf853eb9454239fbf1
javv <vinas.johnandrei@gmail.com>
2026-06-01 13:35:34 +0800

    format only

--- src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductRegisterDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductRegisterDto.java
@@ -1,13 +1,19 @@
 package com.javv.inventorySystem.presentation.product.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
+// @formatter:off
 public record ProductRegisterDto(
-    @NotBlank(message = "Product Register DTO: SKU is required.") String sku,
+    @NotBlank(message = "Product Register DTO: SKU is required.") 
+    String sku,
 
-    @NotBlank(message = "Product Register DTO: Product Name is required.") String name,
+    @NotBlank(message = "Product Register DTO: Product Name is required.") 
+    String name,
 
-    @NotBlank(message = "Product Register DTO: Supplier Name is required.") String supplier,
+    @NotNull(message = "Product Register DTO: Supplier Name is required.") 
+    int supplierId,
 
-    @NotBlank(message = "Product Register DTO: Base Unit of Measure is required.") String baseUnitOfMeasure) {
+    @NotNull(message = "Product Register DTO: Base Unit of Measure is required.") 
+    int baseUnitOfMeasureId) {
 }

dee58f6f84abf17f704837623458c3c02c6d8974
javv <vinas.johnandrei@gmail.com>
2026-06-01 13:35:42 +0800

    format only

--- src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductUpdateDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductUpdateDto.java
@@ -1,13 +1,19 @@
 package com.javv.inventorySystem.presentation.product.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
+// @formatter:off
 public record ProductUpdateDto(
-    @NotBlank(message = "Product Update DTO: SKU is required for update.") String sku,
+    @NotBlank(message = "Product Update DTO: SKU is required for update.") 
+    String sku,
 
-    @NotBlank(message = "Product Update DTO: Name is required for update.") String name,
+    @NotBlank(message = "Product Update DTO: Name is required for update.") 
+    String name,
 
-    @NotBlank(message = "Product Update DTO: Supplier Name is required for update.") String supplier,
+    @NotNull(message = "Product Update DTO: Supplier ID is required for update.") 
+    int supplierId,
 
-    @NotBlank(message = "Product Update DTO: Base Units of Measure is required for update.") String baseUnitOfMeasure) {
+    @NotNull(message = "Product Update DTO: Base Units of Measure ID is required for update.") 
+    int baseUnitOfMeasureId) {
 }

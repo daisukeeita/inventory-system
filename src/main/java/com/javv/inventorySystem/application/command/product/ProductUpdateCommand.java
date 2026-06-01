7c32164ef0873d81300e91090000f0dfd43d67bd
javv <vinas.johnandrei@gmail.com>
2026-06-01 13:31:03 +0800

    changed type to int ID

--- src/main/java/com/javv/inventorySystem/application/command/product/ProductUpdateCommand.java
+++ src/main/java/com/javv/inventorySystem/application/command/product/ProductUpdateCommand.java
@@ -3,7 +3,10 @@ package com.javv.inventorySystem.application.command.product;
 import java.util.Objects;
 
 public record ProductUpdateCommand(
-    String sku, String name, String supplier, String baseUnitOfMeasure) {
+    String sku,
+    String name,
+    int supplierId,
+    int baseUnitOfMeasureId) {
 
   public ProductUpdateCommand {
 
@@ -13,10 +16,10 @@ public record ProductUpdateCommand(
     Objects.requireNonNull(name,
         "Product Update Command Record: Name cannot be null.");
 
-    Objects.requireNonNull(supplier,
-        "Product Update Command Record: Supplier Name cannot be null.");
+    Objects.requireNonNull(supplierId,
+        "Product Update Command Record: Supplier ID cannot be null.");
 
-    Objects.requireNonNull(baseUnitOfMeasure,
+    Objects.requireNonNull(baseUnitOfMeasureId,
         "Product Update Command Record: Base Unit of Measure cannot be null.");
   }
 }

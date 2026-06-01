fb35135ed1f0d91c6f7f62165bba99b5fe9b8ec5
javv <vinas.johnandrei@gmail.com>
2026-06-01 13:35:38 +0800

    format only

--- src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductResponseDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductResponseDto.java
@@ -3,14 +3,28 @@ package com.javv.inventorySystem.presentation.product.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+// @formatter:off
 public record ProductResponseDto(
-    @NotNull(message = "Product Response DTO: ID is required for response.") Long id,
+    @NotNull(message = "Product Response DTO: ID is required for response.") 
+    Long id,
 
-    @NotBlank(message = "Product Response DTO: SKU is required for response.") String sku,
+    @NotBlank(message = "Product Response DTO: SKU is required for response.") 
+    String sku,
 
-    @NotBlank(message = "Product Response DTO: Product Name is required for response.") String name,
+    @NotBlank(message = "Product Response DTO: Product Name is required for response.") 
+    String name,
 
-    @NotBlank(message = "Product Response DTO: Supplier Name is required for response.") String supplier,
+    @NotNull(message = "Product Response DTO: Supplier ID is required for response.")
+    int supplierId,
 
-    @NotBlank(message = "Product Response DTO: Base Unit of Measure is required for response.") String baseUnitOfMeasure) {
+    @NotBlank(message = "Product Response DTO: Supplier Name is required for response.") 
+    String supplierName,
+
+    @NotNull(
+      message = "Product Response DTO: Base Unit of Measure ID is required for response.")
+    int baseUnitOfMeasureId,
+
+    @NotBlank(
+      message = "Product Response DTO: Base Unit of Measure is required for response.") 
+    String baseUnitOfMeasure) {
 }

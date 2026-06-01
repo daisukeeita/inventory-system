d3986aa47f59be2a838f8782feac999db9dc1a89
javv <vinas.johnandrei@gmail.com>
2026-06-02 02:20:52 +0800

    changed blueprint to retrieval only

--- src/main/java/com/javv/inventorySystem/domain/repository/ProductPackagingRepositoryInterface.java
+++ src/main/java/com/javv/inventorySystem/domain/repository/ProductPackagingRepositoryInterface.java
@@ -1,16 +1,13 @@
 package com.javv.inventorySystem.domain.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.javv.inventorySystem.domain.model.product.ProductPackaging;
 
 public interface ProductPackagingRepositoryInterface {
 
-  ProductPackaging save(ProductPackaging productPackaging);
-
-  ProductPackaging update(ProductPackaging productPackaging);
-
-  Optional<ProductPackaging> getByPackagingCode(String packagingCode);
+  List<ProductPackaging> getByProductId(Long productId);
 
   Optional<ProductPackaging> getById(Integer id);
 }

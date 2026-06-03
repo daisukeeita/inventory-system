3d1d06d23ddbed244cf02badf673f1f58f62fe09
javv <vinas.johnandrei@gmail.com>
2026-06-03 09:48:57 +0800

    to be modified

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/productPackaging/ProductPackagingPersistenceMapper.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/productPackaging/ProductPackagingPersistenceMapper.java
@@ -2,12 +2,7 @@ package com.javv.inventorySystem.infrastructure.persistence.productPackaging;
 
 import org.springframework.stereotype.Component;
 
-import com.javv.inventorySystem.domain.model.product.Product;
-import com.javv.inventorySystem.domain.model.product.ProductPackaging;
-import com.javv.inventorySystem.domain.model.product.UnitsOfMeasure;
-import com.javv.inventorySystem.infrastructure.persistence.product.ProductJpaEntity;
 import com.javv.inventorySystem.infrastructure.persistence.product.ProductPersistenceMapper;
-import com.javv.inventorySystem.infrastructure.persistence.unitsOfMeasure.UnitsOfMeasureJpaEntity;
 import com.javv.inventorySystem.infrastructure.persistence.unitsOfMeasure.UnitsOfMeasurePersistenceMapper;
 
 @Component
@@ -23,41 +18,48 @@ public class ProductPackagingPersistenceMapper {
     this.unitsOfMeasurePersistenceMapper = unitsOfMeasurePersistenceMapper;
   }
 
-  public ProductPackagingJpaEntity toJpaEntity(ProductPackaging productPackaging) {
+  // public ProductPackagingJpaEntity toJpaEntity(ProductPackaging
+  // productPackaging) {
+  //
+  // ProductJpaEntity productJpaEntity =
+  // productPersistenceMapper.toJpaEntity(productPackaging.getProductId());
+  //
+  // UnitsOfMeasureJpaEntity unitsOfMeasureJpaEntity =
+  // unitsOfMeasurePersistenceMapper
+  // .toJpaEntity(productPackaging.getUnitsOfMeasureId());
+  //
+  // ProductPackagingJpaEntity productPackagingJpaEntity = new
+  // ProductPackagingJpaEntity();
+  //
+  // // productPackagingJpaEntity.setId(productPackaging.getId());
+  // productPackagingJpaEntity.setPackagingCode(productPackaging.getPackagingCode());
+  // productPackagingJpaEntity.setProduct(productJpaEntity);
+  // productPackagingJpaEntity.setUnitsOfMeasure(unitsOfMeasureJpaEntity);
+  // productPackagingJpaEntity.setConversionFactor(productPackaging.getConversionFactor());
+  // productPackagingJpaEntity.setPrice(productPackaging.getPrice());
+  //
+  // return productPackagingJpaEntity;
+  // }
 
-    ProductJpaEntity productJpaEntity = productPersistenceMapper.toJpaEntity(productPackaging.getProductId());
-
-    UnitsOfMeasureJpaEntity unitsOfMeasureJpaEntity = unitsOfMeasurePersistenceMapper
-        .toJpaEntity(productPackaging.getUnitsOfMeasureId());
-
-    ProductPackagingJpaEntity productPackagingJpaEntity = new ProductPackagingJpaEntity();
-
-    // productPackagingJpaEntity.setId(productPackaging.getId());
-    productPackagingJpaEntity.setPackagingCode(productPackaging.getPackagingCode());
-    productPackagingJpaEntity.setProduct(productJpaEntity);
-    productPackagingJpaEntity.setUnitsOfMeasure(unitsOfMeasureJpaEntity);
-    productPackagingJpaEntity.setConversionFactor(productPackaging.getConversionFactor());
-    productPackagingJpaEntity.setPrice(productPackaging.getPrice());
-
-    return productPackagingJpaEntity;
-  }
-
-  public ProductPackaging toDomainEntity(ProductPackagingJpaEntity productPackagingJpaEntity) {
-
-    Product product = productPersistenceMapper.toDomainEntity(productPackagingJpaEntity.getProduct());
-
-    UnitsOfMeasure unitsOfMeasure = unitsOfMeasurePersistenceMapper.toDomainEntity(
-        productPackagingJpaEntity.getUnitsOfMeasure());
-
-    ProductPackaging productPackaging = new ProductPackaging();
-
-    productPackaging.setId(productPackagingJpaEntity.getId());
-    productPackaging.setPackagingCode(productPackagingJpaEntity.getPackagingCode());
-    productPackaging.setProduct(product);
-    productPackaging.setUnitsOfMeasure(unitsOfMeasure);
-    productPackaging.setConversionFactor(productPackagingJpaEntity.getConversionFactor());
-    productPackaging.setPrice(productPackagingJpaEntity.getPrice());
-
-    return productPackaging;
-  }
+  // public ProductPackaging toDomainEntity(ProductPackagingJpaEntity
+  // productPackagingJpaEntity) {
+  //
+  // Product product =
+  // productPersistenceMapper.toDomainEntity(productPackagingJpaEntity.getProduct());
+  //
+  // UnitsOfMeasure unitsOfMeasure =
+  // unitsOfMeasurePersistenceMapper.toDomainEntity(
+  // productPackagingJpaEntity.getUnitsOfMeasure());
+  //
+  // ProductPackaging productPackaging = new ProductPackaging();
+  //
+  // productPackaging.setId(productPackagingJpaEntity.getId());
+  // productPackaging.setPackagingCode(productPackagingJpaEntity.getPackagingCode());
+  // productPackaging.setProduct(product);
+  // productPackaging.setUnitsOfMeasure(unitsOfMeasure);
+  // productPackaging.setConversionFactor(productPackagingJpaEntity.getConversionFactor());
+  // productPackaging.setPrice(productPackagingJpaEntity.getPrice());
+  //
+  // return productPackaging;
+  // }
 }

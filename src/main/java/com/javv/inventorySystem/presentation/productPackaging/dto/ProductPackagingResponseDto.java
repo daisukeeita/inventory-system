d588356dd4525b048550cc6acbe7d5322ef1a600
javv <vinas.johnandrei@gmail.com>
2026-06-03 09:50:03 +0800

    to be modified

--- src/main/java/com/javv/inventorySystem/presentation/productPackaging/dto/ProductPackagingResponseDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/productPackaging/dto/ProductPackagingResponseDto.java
@@ -5,17 +5,25 @@ import java.math.BigDecimal;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+// @formatter:off
 public record ProductPackagingResponseDto(
-    @NotNull(message = "Product Packaging Response DTO: ID is required for response.") Integer id,
+    @NotNull(message = "Product Packaging Response DTO: ID is required for response.") 
+    Long id,
+
     @NotBlank(message = "Product Packaging Response DTO: Packaging Code is required for response.")
-        String packagingCode,
+    String packagingCode,
+
     @NotBlank(message = "Product Packaging Response DTO: Product SKU is required for response.")
-        String sku,
-    @NotBlank(message = "Product Packaging Response DTO: Product Name is required for response.")
-        String productName,
+    String sku,
+
+    // @NotBlank(message = "Product Packaging Response DTO: Product Name is required for response.")
+    // String productName,
+
     @NotBlank(message = "Product Packaging Response DTO: Unit of Measure is required for response.")
-        String unitOfMeasure,
+    String unitOfMeasure,
+
     @NotNull(message = "Product Packaging Respone DTO: Conversion Factor is required for response.")
-        int conversionFactor,
+    int conversionFactor,
+
     @NotNull(message = "Product Packaging Response DTO: Packaging Price is required for response.")
-        BigDecimal price) {}
+    BigDecimal price) {}

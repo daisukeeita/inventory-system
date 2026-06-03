f9f71261c8f4cbb02d25490ea6258bbf40106dc5
javv <vinas.johnandrei@gmail.com>
2026-06-03 09:50:17 +0800

    to be modified

--- src/main/java/com/javv/inventorySystem/presentation/productPackaging/ProductPackagingDtoMapper.java
+++ src/main/java/com/javv/inventorySystem/presentation/productPackaging/ProductPackagingDtoMapper.java
@@ -1,66 +1,61 @@
 package com.javv.inventorySystem.presentation.productPackaging;
 
-import java.util.Objects;
-
 import org.springframework.stereotype.Component;
-
-import com.javv.inventorySystem.application.command.productPackaging.ProductPackagingRegisterCommand;
-import com.javv.inventorySystem.application.command.productPackaging.ProductPackagingUpdateCommand;
-import com.javv.inventorySystem.domain.model.product.ProductPackaging;
-import com.javv.inventorySystem.presentation.productPackaging.dto.ProductPackagingRegisterDto;
-import com.javv.inventorySystem.presentation.productPackaging.dto.ProductPackagingResponseDto;
-import com.javv.inventorySystem.presentation.productPackaging.dto.ProductPackagingUpdateDto;
 
 @Component
 public class ProductPackagingDtoMapper {
 
-  public ProductPackagingRegisterCommand toRegisterCommand(
-      ProductPackagingRegisterDto productPackagingRegisterDto) {
+  // public ProductPackagingRegisterCommand toRegisterCommand(
+  // ProductPackagingRegisterDto productPackagingRegisterDto) {
+  //
+  // Objects.requireNonNull(
+  // productPackagingRegisterDto,
+  // "Product Packaging DTO Mapper: Cannot map a null ProductPackagingRegisterDto
+  // to a Command"
+  // + " Record.");
+  //
+  // return new ProductPackagingRegisterCommand(
+  // productPackagingRegisterDto.packagingCode(),
+  // productPackagingRegisterDto.sku(),
+  // productPackagingRegisterDto.unitsOfMeasure(),
+  // productPackagingRegisterDto.conversionFactor(),
+  // productPackagingRegisterDto.price());
+  // }
 
-    Objects.requireNonNull(
-        productPackagingRegisterDto,
-        "Product Packaging DTO Mapper: Cannot map a null ProductPackagingRegisterDto to a Command"
-            + " Record.");
+  // public ProductPackagingUpdateCommand toUpdateCommand(
+  // ProductPackagingUpdateDto productPackagingUpdateDto) {
+  //
+  // Objects.requireNonNull(
+  // productPackagingUpdateDto,
+  // "Product Packaging DTO Mapper: Cannot map a null ProductPackagingUpdateDto to
+  // a Command"
+  // + " Record.");
+  //
+  // return new ProductPackagingUpdateCommand(
+  // productPackagingUpdateDto.id(),
+  // productPackagingUpdateDto.packagingCode(),
+  // productPackagingUpdateDto.sku(),
+  // productPackagingUpdateDto.unitOfMeasure(),
+  // productPackagingUpdateDto.conversionFactor(),
+  // productPackagingUpdateDto.price());
+  // }
 
-    return new ProductPackagingRegisterCommand(
-        productPackagingRegisterDto.packagingCode(),
-        productPackagingRegisterDto.sku(),
-        productPackagingRegisterDto.unitsOfMeasure(),
-        productPackagingRegisterDto.conversionFactor(),
-        productPackagingRegisterDto.price());
-  }
-
-  public ProductPackagingUpdateCommand toUpdateCommand(
-      ProductPackagingUpdateDto productPackagingUpdateDto) {
-
-    Objects.requireNonNull(
-        productPackagingUpdateDto,
-        "Product Packaging DTO Mapper: Cannot map a null ProductPackagingUpdateDto to a Command"
-            + " Record.");
-
-    return new ProductPackagingUpdateCommand(
-        productPackagingUpdateDto.id(),
-        productPackagingUpdateDto.packagingCode(),
-        productPackagingUpdateDto.sku(),
-        productPackagingUpdateDto.unitOfMeasure(),
-        productPackagingUpdateDto.conversionFactor(),
-        productPackagingUpdateDto.price());
-  }
-
-  public ProductPackagingResponseDto toResponseDto(ProductPackaging productPackaging) {
-
-    Objects.requireNonNull(
-        productPackaging,
-        "Product Packaging DTO Mapper: Cannot map a null ProductPackaging to a Response DTO"
-            + " Record.");
-
-    return new ProductPackagingResponseDto(
-        productPackaging.getId(),
-        productPackaging.getPackagingCode(),
-        productPackaging.getProductId().getSku(),
-        productPackaging.getProductId().getName(),
-        productPackaging.getUnitsOfMeasureId().getName(),
-        productPackaging.getConversionFactor(),
-        productPackaging.getPrice());
-  }
+  // public ProductPackagingResponseDto toResponseDto(ProductPackaging
+  // productPackaging) {
+  //
+  // Objects.requireNonNull(
+  // productPackaging,
+  // "Product Packaging DTO Mapper: Cannot map a null ProductPackaging to a
+  // Response DTO"
+  // + " Record.");
+  //
+  // return new ProductPackagingResponseDto(
+  // productPackaging.getId(),
+  // productPackaging.getPackagingCode(),
+  // productPackaging.getProductId().getSku(),
+  // productPackaging.getProductId().getName(),
+  // productPackaging.getUnitsOfMeasureId().getName(),
+  // productPackaging.getConversionFactor(),
+  // productPackaging.getPrice());
+  // }
 }

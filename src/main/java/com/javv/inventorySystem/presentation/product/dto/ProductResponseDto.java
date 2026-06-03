8069e03bde7ad7b7737657b867b79d142d4c08da
javv <vinas.johnandrei@gmail.com>
2026-06-03 09:49:24 +0800

    added properties for data responsoe

--- src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductResponseDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductResponseDto.java
@@ -1,5 +1,9 @@
 package com.javv.inventorySystem.presentation.product.dto;
 
+import java.util.List;
+
+import com.javv.inventorySystem.presentation.productPackaging.dto.ProductPackagingResponseDto;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -20,11 +24,13 @@ public record ProductResponseDto(
     @NotBlank(message = "Product Response DTO: Supplier Name is required for response.") 
     String supplierName,
 
-    @NotNull(
-      message = "Product Response DTO: Base Unit of Measure ID is required for response.")
+    @NotNull(message = "Product Response DTO: Base Unit of Measure ID is required for response.")
     int baseUnitOfMeasureId,
 
-    @NotBlank(
-      message = "Product Response DTO: Base Unit of Measure is required for response.") 
-    String baseUnitOfMeasure) {
+    @NotBlank(message = "Product Response DTO: Base Unit of Measure is required for response.") 
+    String baseUnitOfMeasure,
+
+    @NotNull(message = "Product Response DTO: List of Packaging is required for response.")
+    List<ProductPackagingResponseDto> listPackaging
+    ) {
 }

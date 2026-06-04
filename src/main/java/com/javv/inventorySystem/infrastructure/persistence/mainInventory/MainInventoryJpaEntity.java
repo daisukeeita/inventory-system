9ded00192cb0e269c22acf3b82a61e2f973d371d
javv <vinas.johnandrei@gmail.com>
2026-06-04 13:52:39 +0800

    changed product reference from SKU to ID

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/mainInventory/MainInventoryJpaEntity.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/mainInventory/MainInventoryJpaEntity.java
@@ -28,7 +28,7 @@ public class MainInventoryJpaEntity {
   private Integer id;
 
   @OneToOne
-  @JoinColumn(name = "product_sku", referencedColumnName = "sku", nullable = false)
+  @JoinColumn(name = "product_id", referencedColumnName = "id", nullable = false)
   private ProductJpaEntity productJpaEntity;
 
   @Column(name = "quantity_on_hand", nullable = false)
@@ -45,7 +45,8 @@ public class MainInventoryJpaEntity {
   @Column(name = "updated_at", nullable = false)
   private Instant updatedAt;
 
-  public MainInventoryJpaEntity() {}
+  public MainInventoryJpaEntity() {
+  }
 
   public MainInventoryJpaEntity(
       ProductJpaEntity productJpaEntity, int quantityOnHand, int reorderLevel) {

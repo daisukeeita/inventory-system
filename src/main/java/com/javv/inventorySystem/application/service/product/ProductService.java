5ce49cfcad6da29823fb7b18a9b1077477e15dd9
javv <vinas.johnandrei@gmail.com>
2026-06-04 10:45:22 +0800

    updated the service

--- src/main/java/com/javv/inventorySystem/application/service/product/ProductService.java
+++ src/main/java/com/javv/inventorySystem/application/service/product/ProductService.java
@@ -1,18 +1,26 @@
 package com.javv.inventorySystem.application.service.product;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.javv.inventorySystem.application.command.product.ProductRegisterCommand;
+import com.javv.inventorySystem.application.command.product.ProductResponseRead;
 import com.javv.inventorySystem.application.command.product.ProductUpdateCommand;
 import com.javv.inventorySystem.application.command.productPackaging.ProductPackagingRegisterCommand;
+import com.javv.inventorySystem.application.command.productPackaging.ProductPackagingResponseRead;
 import com.javv.inventorySystem.application.service.supplier.SupplierService;
 import com.javv.inventorySystem.domain.exception.EntityAlreadyExistsException;
 import com.javv.inventorySystem.domain.exception.ResourceNotFoundException;
 import com.javv.inventorySystem.domain.model.product.Product;
+import com.javv.inventorySystem.domain.model.product.ProductPackaging;
 import com.javv.inventorySystem.domain.model.product.UnitsOfMeasure;
 import com.javv.inventorySystem.domain.model.supplier.Supplier;
 import com.javv.inventorySystem.domain.repository.ProductRepositoryInterface;
@@ -35,39 +43,19 @@ public class ProductService {
   }
 
   @Transactional
-  public Product saveProduct(ProductRegisterCommand productRegisterCommand) {
+  public ProductResponseRead saveProduct(ProductRegisterCommand productRegisterCommand) {
 
-    Supplier supplier = supplierService
-        .getById(productRegisterCommand.supplierId());
-
-    UnitsOfMeasure unitsOfMeasure = unitsOfMeasureService
-        .getById(productRegisterCommand.baseUnitOfMeasureId());
-
-    Product product = new Product();
-    product.setSku(productRegisterCommand.sku());
-    product.setName(productRegisterCommand.name());
-    product.setSupplierId(supplier.getId());
-    product.setBaseUnitsOfMeasureId(unitsOfMeasure.getId());
-
-    for (ProductPackagingRegisterCommand packaging : productRegisterCommand.listPackagingCommand()) {
-      UnitsOfMeasure unitMeasure = unitsOfMeasureService.getById(
-          packaging.unitOfMeasureId());
-
-      product.addPackaging(
-          packaging.packagingCode(),
-          unitMeasure.getId(),
-          packaging.conversionFactor(),
-          packaging.price());
-    }
+    Product product = toDomainEntity(productRegisterCommand);
 
     try {
-      return productRepositoryInterface.save(product);
+      return toProductResponseRead(productRepositoryInterface.save(product));
     } catch (DataIntegrityViolationException exception) {
       throw new EntityAlreadyExistsException(
           "Product Service: " + exception.getMessage());
     }
   }
 
+  // TODO: Modify the updateProduct method
   @Transactional
   public Product updateProduct(Long id, ProductUpdateCommand productUpdateCommand) {
     Supplier supplier = supplierService.getById(productUpdateCommand.supplierId());
@@ -107,4 +95,74 @@ public class ProductService {
 
     return product;
   }
+
+  public Page<Product> getPageableProduct(int page, int size) {
+    Pageable pageable = PageRequest.of(page, size);
+
+    return productRepositoryInterface.getPageableProduct(pageable);
+  }
+
+  private ProductResponseRead toProductResponseRead(Product product) {
+    String supplierName = supplierService.getById(
+        product.getSupplierId()).getCompanyName();
+    String baseUnitOfMeasureName = unitsOfMeasureService.getById(
+        product.getBaseUnitsOfMeasureId()).getName();
+
+    List<ProductPackagingResponseRead> listProductPackagingRead = new ArrayList<>();
+
+    for (ProductPackaging packaging : product.getListPackages()) {
+      String productSku = getById(packaging.getProductId()).getSku();
+      String unitOfMeasureName = unitsOfMeasureService.getById(
+          packaging.getUnitsOfMeasureId()).getName();
+
+      listProductPackagingRead.add(
+          new ProductPackagingResponseRead(
+              packaging.getId(),
+              packaging.getPackagingCode(),
+              productSku,
+              unitOfMeasureName,
+              packaging.getConversionFactor(),
+              packaging.getPrice()));
+    }
+
+    ProductResponseRead responseRead = new ProductResponseRead(
+        product.getId(),
+        product.getSku(),
+        product.getName(),
+        product.getSupplierId(),
+        supplierName,
+        product.getBaseUnitsOfMeasureId(),
+        baseUnitOfMeasureName,
+        listProductPackagingRead);
+
+    return responseRead;
+  }
+
+  private Product toDomainEntity(ProductRegisterCommand productRegisterCommand) {
+    Supplier supplier = supplierService
+        .getById(productRegisterCommand.supplierId());
+
+    UnitsOfMeasure unitsOfMeasure = unitsOfMeasureService
+        .getById(productRegisterCommand.baseUnitOfMeasureId());
+
+    Product product = new Product();
+    product.setSku(productRegisterCommand.sku());
+    product.setName(productRegisterCommand.name());
+    product.setSupplierId(supplier.getId());
+    product.setBaseUnitsOfMeasureId(unitsOfMeasure.getId());
+
+    for (ProductPackagingRegisterCommand packaging : productRegisterCommand.listPackagingCommand()) {
+      UnitsOfMeasure unitMeasure = unitsOfMeasureService.getById(
+          packaging.unitOfMeasureId());
+
+      product.addPackaging(
+          packaging.packagingCode(),
+          unitMeasure.getId(),
+          packaging.conversionFactor(),
+          packaging.price());
+    }
+
+    return product;
+  }
+
 }

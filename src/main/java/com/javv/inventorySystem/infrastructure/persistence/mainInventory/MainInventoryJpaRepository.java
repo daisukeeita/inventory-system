7ba08246fb66b280a95da6827851294f456eb66d
javv <vinas.johnandrei@gmail.com>
2026-06-04 13:52:48 +0800

    meh

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/mainInventory/MainInventoryJpaRepository.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/mainInventory/MainInventoryJpaRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MainInventoryJpaRepository extends JpaRepository<MainInventoryJpaEntity, Integer> {
 
   Optional<MainInventoryJpaEntity> findByProductJpaEntitySku(String sku);
+
 }

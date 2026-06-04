6624f2560298d1bb2ea49d5ca4692c8eaf389bc1
javv <vinas.johnandrei@gmail.com>
2026-06-04 13:53:43 +0800

    updated register DTO

--- src/main/java/com/javv/inventorySystem/presentation/mainInventory/dto/MainInventoryRegisterDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/mainInventory/dto/MainInventoryRegisterDto.java
@@ -4,16 +4,14 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+// @formatter:off
 public record MainInventoryRegisterDto(
-    @NotBlank(message = "Main Inventory Register DTO: Product SKU is required for registration.")
-        String sku,
-    @NotNull(message = "Main Inventory Register DTO: Quantity is required for registration.")
-        @Min(
-            value = 0,
-            message = "Main Inventory Register DTO: Quantity should not be less than 0.")
-        int quantityOnHand,
-    @NotNull(message = "Main Inventory Register DTO: Reorder Level is required for registration.")
-        @Min(
-            value = 0,
-            message = "Main Inventory Register DTO: Reorder Level should not be less than 0.")
-        int reorderLevel) {}
+    @NotBlank(message = "Main Inventory Register DTO: Product ID is required for registration.") 
+    Long productId,
+
+    @NotNull(message = "Main Inventory Register DTO: Quantity is required for registration.") @Min(value = 0, message = "Main Inventory Register DTO: Quantity should not be less than 0.") 
+    int quantityOnHand,
+
+    @NotNull(message = "Main Inventory Register DTO: Reorder Level is required for registration.") @Min(value = 0, message = "Main Inventory Register DTO: Reorder Level should not be less than 0.") 
+    int reorderLevel) {
+}

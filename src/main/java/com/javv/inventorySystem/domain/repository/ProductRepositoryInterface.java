d74aa36ced6dfe00e0482649dd4cfa4c511a0d88
javv <vinas.johnandrei@gmail.com>
2026-06-04 10:45:36 +0800

    added pagination for products

--- src/main/java/com/javv/inventorySystem/domain/repository/ProductRepositoryInterface.java
+++ src/main/java/com/javv/inventorySystem/domain/repository/ProductRepositoryInterface.java
@@ -2,6 +2,9 @@ package com.javv.inventorySystem.domain.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.javv.inventorySystem.domain.model.product.Product;
 
 public interface ProductRepositoryInterface {
@@ -12,4 +15,6 @@ public interface ProductRepositoryInterface {
   Optional<Product> getBySku(String sku);
 
   Optional<Product> getById(Long id);
+
+  Page<Product> getPageableProduct(Pageable pageable);
 }

e221a5056ff7a1c6492e36a0781b5b1e6a133e23
javv <vinas.johnandrei@gmail.com>
2026-06-04 13:51:17 +0800

    added Update Command for Main Inventory

---
+++ src/main/java/com/javv/inventorySystem/application/command/mainInventory/MainInventoryUpdateCommand.java
@@ -0,0 +1,31 @@
+package com.javv.inventorySystem.application.command.mainInventory;
+
+import java.util.Objects;
+
+public record MainInventoryUpdateCommand(
+    int id, Long productId, int quantityOnHand, int reorderLevel) {
+
+  public MainInventoryUpdateCommand {
+    Objects.requireNonNull(
+        productId,
+        "Main Inventory Register Command: Product ID cannot be null.");
+
+    Objects.requireNonNull(
+        quantityOnHand,
+        "Main Inventory Register Command: Quantity on Hand cannot be null.");
+    Objects.requireNonNull(
+        reorderLevel,
+        "Main Inventory Register Command: Reorder Level cannot be null.");
+
+    if (quantityOnHand < 0) {
+      throw new IllegalArgumentException(
+          "Main Inventory Register Command: Quantity on Hand cannot be less than zero.");
+    }
+
+    if (reorderLevel < 0) {
+      throw new IllegalArgumentException(
+          "Main Inventory Register Command: Reorder Level cannot be less than zero.");
+    }
+  }
+
+}

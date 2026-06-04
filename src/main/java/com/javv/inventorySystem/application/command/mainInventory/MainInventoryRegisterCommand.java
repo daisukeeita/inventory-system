2ba8947bfca7c47c295dbf1ae668761d1140563d
javv <vinas.johnandrei@gmail.com>
2026-06-04 13:51:29 +0800

    updated productId as reference

--- src/main/java/com/javv/inventorySystem/application/command/mainInventory/MainInventoryRegisterCommand.java
+++ src/main/java/com/javv/inventorySystem/application/command/mainInventory/MainInventoryRegisterCommand.java
@@ -2,13 +2,18 @@ package com.javv.inventorySystem.application.command.mainInventory;
 
 import java.util.Objects;
 
-public record MainInventoryRegisterCommand(String sku, int quantityOnHand, int reorderLevel) {
+public record MainInventoryRegisterCommand(Long productId, int quantityOnHand, int reorderLevel) {
   public MainInventoryRegisterCommand {
-    Objects.requireNonNull(sku, "Main Inventory Register Command: Product SKU cannot be null.");
     Objects.requireNonNull(
-        quantityOnHand, "Main Inventory Register Command: Quantity on Hand cannot be null.");
+        productId,
+        "Main Inventory Register Command: Product ID cannot be null.");
+
     Objects.requireNonNull(
-        reorderLevel, "Main Inventory Register Command: Reorder Level cannot be null.");
+        quantityOnHand,
+        "Main Inventory Register Command: Quantity on Hand cannot be null.");
+    Objects.requireNonNull(
+        reorderLevel,
+        "Main Inventory Register Command: Reorder Level cannot be null.");
 
     if (quantityOnHand < 0) {
       throw new IllegalArgumentException(
@@ -18,11 +23,6 @@ public record MainInventoryRegisterCommand(String sku, int quantityOnHand, int r
     if (reorderLevel < 0) {
       throw new IllegalArgumentException(
           "Main Inventory Register Command: Reorder Level cannot be less than zero.");
-    }
-
-    if (sku.isBlank()) {
-      throw new IllegalArgumentException(
-          "Main Inventory Register Command: Product SKU cannot be blank.");
     }
   }
 }

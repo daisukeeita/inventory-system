ed0b3f84f904fcfbf2dde811fe682742af2c5fd3
javv <vinas.johnandrei@gmail.com>
2026-06-11 22:20:55 +0800

    change thrown exception to custom Object Mapping Exception

--- src/main/java/com/javv/inventorySystem/presentation/supplier/SupplierDtoMapper.java
+++ src/main/java/com/javv/inventorySystem/presentation/supplier/SupplierDtoMapper.java
@@ -1,11 +1,10 @@
 package com.javv.inventorySystem.presentation.supplier;
 
-import java.util.Objects;
-
 import org.springframework.stereotype.Component;
 
 import com.javv.inventorySystem.application.command.supplier.SupplierRegisterCommand;
 import com.javv.inventorySystem.application.command.supplier.SupplierUpdateCommand;
+import com.javv.inventorySystem.domain.exception.ObjectMappingException;
 import com.javv.inventorySystem.domain.model.supplier.Supplier;
 import com.javv.inventorySystem.presentation.supplier.dto.SupplierRegisterDto;
 import com.javv.inventorySystem.presentation.supplier.dto.SupplierResponseDto;
@@ -16,8 +15,10 @@ public class SupplierDtoMapper {
 
   public SupplierRegisterCommand toRegisterCommandRecord(SupplierRegisterDto supplierRegisterDto) {
 
-    Objects.requireNonNull(supplierRegisterDto,
-        "Supplier DTO Mapper: Cannot map a null SupplierRegisterDto to a Command Record.");
+    if (supplierRegisterDto == null) {
+      throw new ObjectMappingException(
+          "Supplier DTO Mapper: Cannot map a null SupplierRegisterDto to a Command Record.");
+    }
 
     return new SupplierRegisterCommand(
         supplierRegisterDto.companyName(),
@@ -34,8 +35,10 @@ public class SupplierDtoMapper {
   public SupplierUpdateCommand toUpdateCommandRecord(
       SupplierUpdateDto supplierUpdateDto) {
 
-    Objects.requireNonNull(supplierUpdateDto,
-        "Supplier DTO Mapper: Cannot map a null SupplierUpdateAddressDto to a Command Record.");
+    if (supplierUpdateDto == null) {
+      throw new ObjectMappingException(
+          "Supplier DTO Mapper: Cannot map a null SupplierUpdateAddressDto to a Command Record.");
+    }
 
     return new SupplierUpdateCommand(
         supplierUpdateDto.companyName(),
@@ -51,8 +54,10 @@ public class SupplierDtoMapper {
 
   public SupplierResponseDto toResponseDto(Supplier supplier) {
 
-    Objects.requireNonNull(supplier,
-        "Supplier DTO Mapper: Cannot map a null Supplier to a Response DTO.");
+    if (supplier == null) {
+      throw new ObjectMappingException(
+          "Supplier DTO Mapper: Cannot map a null Supplier to a Response DTO.");
+    }
 
     String address = supplier.getSupplierAddress().getStreet()
         + ", "

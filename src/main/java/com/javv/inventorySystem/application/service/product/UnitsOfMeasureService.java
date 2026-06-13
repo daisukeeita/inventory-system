ad727333af57023fbf46e1b7b1a805d51a86ff15
javv <vinas.johnandrei@gmail.com>
2026-06-13 21:33:46 +0800

    changed approach of methods for units of measure

--- src/main/java/com/javv/inventorySystem/application/service/product/UnitsOfMeasureService.java
+++ src/main/java/com/javv/inventorySystem/application/service/product/UnitsOfMeasureService.java
@@ -1,6 +1,5 @@
 package com.javv.inventorySystem.application.service.product;
 
-import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.dao.DataIntegrityViolationException;
@@ -9,8 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.javv.inventorySystem.application.command.unitsOfMeasure.UnitsOfMeasureRegisterCommand;
 import com.javv.inventorySystem.application.command.unitsOfMeasure.UnitsOfMeasureUpdateCommand;
-import com.javv.inventorySystem.domain.exception.EntityAlreadyExistsException;
+import com.javv.inventorySystem.domain.exception.ObjectMappingException;
 import com.javv.inventorySystem.domain.exception.ResourceNotFoundException;
+import com.javv.inventorySystem.domain.exception.ServiceOperationException;
 import com.javv.inventorySystem.domain.model.product.UnitsOfMeasure;
 import com.javv.inventorySystem.domain.repository.UnitsOfMeasureRepositoryInterface;
 
@@ -27,61 +27,62 @@ public class UnitsOfMeasureService {
 
   @Transactional
   public UnitsOfMeasure saveMeasure(UnitsOfMeasureRegisterCommand unitsOfMeasureRegisterCommand) {
-    UnitsOfMeasure unitsOfMeasure = registerToDomainEntity(unitsOfMeasureRegisterCommand);
+    UnitsOfMeasure unitsOfMeasure = toDomainEntity(unitsOfMeasureRegisterCommand);
 
     try {
       UnitsOfMeasure savedUnitsOfMeasure = unitsOfMeasureRepositoryInterface.save(unitsOfMeasure);
       return savedUnitsOfMeasure;
 
     } catch (DataIntegrityViolationException exception) {
-      throw new EntityAlreadyExistsException("Service: Unit of Measure is already registered.");
+      throw new ServiceOperationException(
+          "Units of Measure Creation Failed: The provided details conflict with an existing units of measure.");
     }
   }
 
   @Transactional
   public UnitsOfMeasure updateMeasure(
       Integer id, UnitsOfMeasureUpdateCommand unitsOfMeasureUpdateCommand) {
-    Optional<UnitsOfMeasure> optionalMeasure = unitsOfMeasureRepositoryInterface.getById(id);
-
-    UnitsOfMeasure unitsOfMeasure =
-        optionalMeasure.orElseThrow(
-            () ->
-                new ResourceNotFoundException(
-                    "Service: Units of Measure not found by id: '" + id + "'"));
-
-    unitsOfMeasure.setName(unitsOfMeasureUpdateCommand.name());
-    unitsOfMeasure.setAbbreviation(unitsOfMeasureUpdateCommand.abbreviation());
 
     try {
+      UnitsOfMeasure unitsOfMeasure = getById(id);
+
+      unitsOfMeasure.setName(unitsOfMeasureUpdateCommand.name());
+      unitsOfMeasure.setAbbreviation(unitsOfMeasureUpdateCommand.abbreviation());
+
       UnitsOfMeasure savedUnitsOfMeasure = unitsOfMeasureRepositoryInterface.update(unitsOfMeasure);
+
       return savedUnitsOfMeasure;
 
     } catch (DataIntegrityViolationException exception) {
-      throw new EntityAlreadyExistsException(
-          "This unit of measure is already registered.", exception);
+      throw new ServiceOperationException(
+          "Units of Measure Update Failed: The provided details conflict with an existing units of measure.");
+    } catch (ResourceNotFoundException exception) {
+      throw new ServiceOperationException(
+          "Units of Measure Update Failed: " + exception.getMessage() + ".");
     }
   }
 
   public UnitsOfMeasure getById(int id) {
-    Optional<UnitsOfMeasure> optionalUnitsOfMeasure = unitsOfMeasureRepositoryInterface.getById(id);
+    Optional<UnitsOfMeasure> optionalUnitsOfMeasure = unitsOfMeasureRepositoryInterface.findById(id);
 
     return optionalUnitsOfMeasure.orElseThrow(
-        () -> new ResourceNotFoundException("Units of Measure not found by id: ;" + id + "'"));
+        () -> new ResourceNotFoundException("Units of Measure not found by id: " + id + "."));
   }
 
   public UnitsOfMeasure getByName(String name) {
-    Optional<UnitsOfMeasure> optionalUnitsOfMeasure =
-        unitsOfMeasureRepositoryInterface.getByName(name);
+    Optional<UnitsOfMeasure> optionalUnitsOfMeasure = unitsOfMeasureRepositoryInterface.findByName(name);
 
     return optionalUnitsOfMeasure.orElseThrow(
-        () -> new ResourceNotFoundException("Units of Measure not found by name: '" + name + "'"));
+        () -> new ResourceNotFoundException("Units of Measure not found by name: " + name + "."));
   }
 
-  private UnitsOfMeasure registerToDomainEntity(
+  private UnitsOfMeasure toDomainEntity(
       UnitsOfMeasureRegisterCommand unitsOfMeasureRegisterCommand) {
-    Objects.requireNonNull(
-        unitsOfMeasureRegisterCommand,
-        "Cannot map a null UnitsOfMeasureRegisterCommand to a Domain Entity.");
+
+    if (unitsOfMeasureRegisterCommand == null) {
+      throw new ObjectMappingException(
+          "Units of Measure Service: Cannot map a null UnitsOfMeasureRegisterCommand to a Domain Entity.");
+    }
 
     UnitsOfMeasure unitsOfMeasure = new UnitsOfMeasure();
     unitsOfMeasure.setName(unitsOfMeasureRegisterCommand.name());

ca3e05d63dc70c7a88a1a8ee2c5e0149d5e2806c
javv <vinas.johnandrei@gmail.com>
2026-06-13 21:35:14 +0800

    format modification

--- src/main/java/com/javv/inventorySystem/presentation/unitsOfMeasure/dto/UnitsOfMeasureUpdateDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/unitsOfMeasure/dto/UnitsOfMeasureUpdateDto.java
@@ -2,7 +2,10 @@ package com.javv.inventorySystem.presentation.unitsOfMeasure.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
+// @formatter:off
 public record UnitsOfMeasureUpdateDto(
-    @NotBlank(message = "Measure Update DTO: Name is required to update.") String name,
+    @NotBlank(message = "Measure Update DTO: Name is required to update.") 
+    String name,
+
     @NotBlank(message = "Measure Update DTO: Abbreviation is required to update")
-        String abbreviation) {}
+    String abbreviation) {}

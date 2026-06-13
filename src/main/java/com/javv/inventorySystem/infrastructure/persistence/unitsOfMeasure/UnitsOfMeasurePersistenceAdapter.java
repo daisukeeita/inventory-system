4565698abadd971acd2a47e713f29f6ce754ef7f
javv <vinas.johnandrei@gmail.com>
2026-06-13 21:34:34 +0800

    added page type retrieval method and updated contents of method

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/unitsOfMeasure/UnitsOfMeasurePersistenceAdapter.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/unitsOfMeasure/UnitsOfMeasurePersistenceAdapter.java
@@ -2,8 +2,11 @@ package com.javv.inventorySystem.infrastructure.persistence.unitsOfMeasure;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import com.javv.inventorySystem.domain.exception.ResourceNotFoundException;
 import com.javv.inventorySystem.domain.model.product.UnitsOfMeasure;
 import com.javv.inventorySystem.domain.repository.UnitsOfMeasureRepositoryInterface;
 
@@ -22,36 +25,46 @@ public class UnitsOfMeasurePersistenceAdapter implements UnitsOfMeasureRepositor
 
   @Override
   public UnitsOfMeasure save(UnitsOfMeasure unitsOfMeasure) {
-    UnitsOfMeasureJpaEntity unitsOfMeasureJpaEntity =
-        unitsOfMeasurePersistenceMapper.toJpaEntity(unitsOfMeasure);
+    UnitsOfMeasureJpaEntity unitsOfMeasureJpaEntity = unitsOfMeasurePersistenceMapper.toJpaEntity(unitsOfMeasure);
 
-    UnitsOfMeasureJpaEntity savedEntity = unitsOfMeasureJpaRepository.save(unitsOfMeasureJpaEntity);
+    UnitsOfMeasureJpaEntity savedEntity = unitsOfMeasureJpaRepository.saveAndFlush(unitsOfMeasureJpaEntity);
 
     return unitsOfMeasurePersistenceMapper.toDomainEntity(savedEntity);
   }
 
   @Override
   public UnitsOfMeasure update(UnitsOfMeasure unitsOfMeasure) {
-    UnitsOfMeasureJpaEntity unitsOfMeasureJpaEntity =
-        unitsOfMeasurePersistenceMapper.toJpaEntity(unitsOfMeasure);
 
-    UnitsOfMeasureJpaEntity savedEntity =
-        unitsOfMeasureJpaRepository.saveAndFlush(unitsOfMeasureJpaEntity);
+    UnitsOfMeasureJpaEntity fetchedEntity = unitsOfMeasureJpaRepository
+        .findById(unitsOfMeasure.getId())
+        .orElseThrow(() -> new ResourceNotFoundException(
+            "Units of Measure Persistence: Units of Measure not found using ID: " + unitsOfMeasure.getId() + "."));
+
+    UnitsOfMeasureJpaEntity updatedJpaEntity = unitsOfMeasurePersistenceMapper
+        .updateJpaEntity(unitsOfMeasure, fetchedEntity);
+
+    UnitsOfMeasureJpaEntity savedEntity = unitsOfMeasureJpaRepository.saveAndFlush(updatedJpaEntity);
 
     return unitsOfMeasurePersistenceMapper.toDomainEntity(savedEntity);
   }
 
   @Override
-  public Optional<UnitsOfMeasure> getById(Integer id) {
+  public Optional<UnitsOfMeasure> findById(int id) {
     Optional<UnitsOfMeasureJpaEntity> optionalEntity = unitsOfMeasureJpaRepository.findById(id);
 
     return optionalEntity.map(entity -> unitsOfMeasurePersistenceMapper.toDomainEntity(entity));
   }
 
   @Override
-  public Optional<UnitsOfMeasure> getByName(String name) {
+  public Optional<UnitsOfMeasure> findByName(String name) {
     Optional<UnitsOfMeasureJpaEntity> optionalEntity = unitsOfMeasureJpaRepository.findByName(name);
 
     return optionalEntity.map(entity -> unitsOfMeasurePersistenceMapper.toDomainEntity(entity));
+  }
+
+  @Override
+  public Page<UnitsOfMeasure> findAll(Pageable pageable) {
+    // TODO Auto-generated method stub
+    return null;
   }
 }

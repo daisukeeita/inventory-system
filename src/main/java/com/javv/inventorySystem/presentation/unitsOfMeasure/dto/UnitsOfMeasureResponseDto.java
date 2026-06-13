319f953a128add39992f8beb52a480d366fa8695
javv <vinas.johnandrei@gmail.com>
2026-06-13 21:35:09 +0800

    format modification

--- src/main/java/com/javv/inventorySystem/presentation/unitsOfMeasure/dto/UnitsOfMeasureResponseDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/unitsOfMeasure/dto/UnitsOfMeasureResponseDto.java
@@ -5,12 +5,19 @@ import java.time.Instant;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+// @formatter:off
 public record UnitsOfMeasureResponseDto(
-    @NotNull(message = "Measure Response DTO: Measure ID is required for response.") Integer id,
-    @NotBlank(message = "Measure Response DTO: Measure Name is required for response.") String name,
+    @NotNull(message = "Measure Response DTO: Measure ID is required for response.") 
+    Integer id,
+
+    @NotBlank(message = "Measure Response DTO: Measure Name is required for response.") 
+    String name,
+
     @NotBlank(message = "Measure Response DTO: Measure Abbreviation is required for response.")
-        String abbreviation,
+    String abbreviation,
+
     @NotNull(message = "Measure Response DTO: Measure Created At is required for response.")
-        Instant createdAt,
+    Instant createdAt,
+
     @NotNull(message = "Measure Response DTO: Measure Updated At is required for response.")
-        Instant updatedAt) {}
+    Instant updatedAt) {}

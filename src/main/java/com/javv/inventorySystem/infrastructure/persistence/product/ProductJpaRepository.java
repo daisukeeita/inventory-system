bd7fac3b60bcdbc9e702b20c84e91fdbe82fd8d1
javv <vinas.johnandrei@gmail.com>
2026-06-15 13:47:02 +0800

    aded batch exists checker

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/product/ProductJpaRepository.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/product/ProductJpaRepository.java
@@ -1,5 +1,6 @@
 package com.javv.inventorySystem.infrastructure.persistence.product;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProductJpaRepository extends JpaRepository<ProductJpaEntity, Long> {
 
   Optional<ProductJpaEntity> findBySku(String sku);
+
+  Long countByIdIn(List<Long> listId);
 }

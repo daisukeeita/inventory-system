935da50bbe0f8c092c59fa1b3cf5251c139dbad1
javv <vinas.johnandrei@gmail.com>
2026-06-16 13:23:13 +0800

    modified retrieve method

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/transaction/inbound/InboundJpaRepository.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/transaction/inbound/InboundJpaRepository.java
@@ -3,9 +3,13 @@ package com.javv.inventorySystem.infrastructure.persistence.transaction.inbound;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InboundJpaRepository extends JpaRepository<InboundJpaEntity, Long> {
 
   Optional<InboundJpaEntity> findByDateReceived(LocalDateTime dateReceived);
+
+  Page<InboundJpaEntity> findAll(Pageable pageable);
 }

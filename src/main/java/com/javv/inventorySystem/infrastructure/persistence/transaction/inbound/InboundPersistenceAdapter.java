460ea4def6dc67a615ced91c1d372bfe079be15e
javv <vinas.johnandrei@gmail.com>
2026-06-16 20:53:38 +0800

    moved entity transformation to mapper

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/transaction/inbound/InboundPersistenceAdapter.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/transaction/inbound/InboundPersistenceAdapter.java
@@ -10,15 +10,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import com.javv.inventorySystem.domain.model.transaction.inbound.Inbound;
-import com.javv.inventorySystem.domain.model.transaction.inbound.InboundItem;
 import com.javv.inventorySystem.domain.repository.InboundRepositoryInterface;
-import com.javv.inventorySystem.infrastructure.persistence.product.ProductJpaEntity;
 import com.javv.inventorySystem.infrastructure.persistence.product.ProductJpaRepository;
-import com.javv.inventorySystem.infrastructure.persistence.productPackaging.ProductPackagingJpaEntity;
 import com.javv.inventorySystem.infrastructure.persistence.productPackaging.ProductPackagingJpaRepository;
-import com.javv.inventorySystem.infrastructure.persistence.supplier.SupplierJpaEntity;
 import com.javv.inventorySystem.infrastructure.persistence.supplier.SupplierJpaRepository;
-import com.javv.inventorySystem.infrastructure.persistence.user.UserJpaEntity;
 import com.javv.inventorySystem.infrastructure.persistence.user.UserJpaRepository;
 
 @Repository
@@ -39,58 +34,17 @@ public class InboundPersistenceAdapter implements InboundRepositoryInterface {
   @Autowired
   private ProductPackagingJpaRepository productPackagingJpaRepository;
 
+  @Autowired
+  private InboundPersistenceMapper inboundPersistenceMapper;
+
   @Override
   public Inbound save(Inbound inbound) {
 
-    UserJpaEntity userJpaEntity = userJpaRepository.getReferenceById(
-        inbound.getEncoderId());
-
-    SupplierJpaEntity supplierJpaEntity = supplierJpaRepository.getReferenceById(
-        inbound.getSupplierId());
-
-    InboundJpaEntity inboundJpaEntity = new InboundJpaEntity();
-    inboundJpaEntity.setSupplierJpaEntity(supplierJpaEntity);
-    inboundJpaEntity.setUserJpaEntity(userJpaEntity);
-    inboundJpaEntity.setInvoiceNumber(
-        inbound.getInvoiceNumber());
-    inboundJpaEntity.setDateReceived(
-        inbound.getDateReceived());
-
-    for (InboundItem item : inbound.getListInboundItem()) {
-
-      ProductJpaEntity productJpaEntity = productJpaRepository
-          .getReferenceBySku(item.getProductSku());
-
-      ProductPackagingJpaEntity productPackagingJpaEntity = productPackagingJpaRepository
-          .getReferenceById(item.getPackagingId());
-
-      inboundJpaEntity.addItem(
-          productJpaEntity,
-          productPackagingJpaEntity,
-          item.getQuantityReceived(),
-          item.getBaseQuantityEquivalent());
-    }
+    InboundJpaEntity inboundJpaEntity = inboundPersistenceMapper.toJpaEntity(inbound);
 
     InboundJpaEntity savedEntity = inboundJpaRepository.saveAndFlush(inboundJpaEntity);
 
-    Inbound inboundDomain = new Inbound();
-    inboundDomain.setId(savedEntity.getId());
-    inboundDomain.setEncoderId(savedEntity.getUserJpaEntity().getUserId());
-    inboundDomain.setSupplierId(savedEntity.getSupplierJpaEntity().getId());
-    inboundDomain.setInvoiceNumber(savedEntity.getInvoiceNumber());
-    inboundDomain.setDateReceived(savedEntity.getDateReceived());
-    inboundDomain.setCreatedAt(savedEntity.getCreatedAt());
-    inboundDomain.setUpdatedAt(savedEntity.getUpdatedAt());
-
-    for (InboundItemJpaEntity itemJpa : savedEntity.getListInboundItem()) {
-      inboundDomain.addInboundItem(
-          itemJpa.getProduct().getSku(),
-          itemJpa.getProductPackagingJpaEntity().getId(),
-          itemJpa.getQuantityReceived(),
-          itemJpa.getBaseQuantityEquivalent());
-    }
-
-    return inboundDomain;
+    return inboundPersistenceMapper.toDomainEntity(savedEntity);
   }
 
   @Override

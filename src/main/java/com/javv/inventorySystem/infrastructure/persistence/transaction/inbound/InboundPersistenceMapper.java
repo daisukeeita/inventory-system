27d4849dde600005756d92d8c75f761932abe8ef
javv <vinas.johnandrei@gmail.com>
2026-06-16 20:53:57 +0800

    updated toDomain and toJpa methods

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/transaction/inbound/InboundPersistenceMapper.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/transaction/inbound/InboundPersistenceMapper.java
@@ -1,39 +1,90 @@
 package com.javv.inventorySystem.infrastructure.persistence.transaction.inbound;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.javv.inventorySystem.domain.model.transaction.inbound.Inbound;
-import com.javv.inventorySystem.infrastructure.persistence.product.ProductPersistenceMapper;
-import com.javv.inventorySystem.infrastructure.persistence.productPackaging.ProductPackagingPersistenceMapper;
-import com.javv.inventorySystem.infrastructure.persistence.supplier.SupplierPersistenceMapper;
-import com.javv.inventorySystem.infrastructure.persistence.user.UserPersistenceMapper;
+import com.javv.inventorySystem.domain.model.transaction.inbound.InboundItem;
+import com.javv.inventorySystem.infrastructure.persistence.product.ProductJpaEntity;
+import com.javv.inventorySystem.infrastructure.persistence.product.ProductJpaRepository;
+import com.javv.inventorySystem.infrastructure.persistence.productPackaging.ProductPackagingJpaEntity;
+import com.javv.inventorySystem.infrastructure.persistence.productPackaging.ProductPackagingJpaRepository;
+import com.javv.inventorySystem.infrastructure.persistence.supplier.SupplierJpaEntity;
+import com.javv.inventorySystem.infrastructure.persistence.supplier.SupplierJpaRepository;
+import com.javv.inventorySystem.infrastructure.persistence.user.UserJpaEntity;
+import com.javv.inventorySystem.infrastructure.persistence.user.UserJpaRepository;
 
 @Component
 public class InboundPersistenceMapper {
 
-  private UserPersistenceMapper userPersistenceMapper;
-  private ProductPersistenceMapper productPersistenceMapper;
-  private SupplierPersistenceMapper supplierPersistenceMapper;
-  private ProductPackagingPersistenceMapper productPackagingPersistenceMapper;
+  @Autowired
+  private UserJpaRepository userJpaRepository;
 
-  public InboundPersistenceMapper(
-      UserPersistenceMapper userPersistenceMapper,
-      ProductPersistenceMapper productPersistenceMapper,
-      SupplierPersistenceMapper supplierPersistenceMapper,
-      ProductPackagingPersistenceMapper productPackagingPersistenceMapper) {
-    this.userPersistenceMapper = userPersistenceMapper;
-    this.productPersistenceMapper = productPersistenceMapper;
-    this.supplierPersistenceMapper = supplierPersistenceMapper;
-    this.productPackagingPersistenceMapper = productPackagingPersistenceMapper;
-  }
+  @Autowired
+  private SupplierJpaRepository supplierJpaRepository;
+
+  @Autowired
+  private ProductJpaRepository productJpaRepository;
+
+  @Autowired
+  private ProductPackagingJpaRepository productPackagingJpaRepository;
 
   public InboundJpaEntity toJpaEntity(Inbound inbound) {
 
-    return null;
+    UserJpaEntity userJpaEntity = userJpaRepository.getReferenceById(
+        inbound.getEncoderId());
+
+    SupplierJpaEntity supplierJpaEntity = supplierJpaRepository.getReferenceById(
+        inbound.getSupplierId());
+
+    InboundJpaEntity inboundJpaEntity = new InboundJpaEntity();
+    inboundJpaEntity.setSupplierJpaEntity(supplierJpaEntity);
+    inboundJpaEntity.setUserJpaEntity(userJpaEntity);
+    inboundJpaEntity.setInvoiceNumber(inbound.getInvoiceNumber());
+    inboundJpaEntity.setDateReceived(inbound.getDateReceived());
+
+    for (InboundItem inboundItem : inbound.getListInboundItem()) {
+
+      ProductJpaEntity productJpaEntity = productJpaRepository
+          .getReferenceBySku(inboundItem.getProductSku());
+
+      ProductPackagingJpaEntity productPackagingJpaEntity = productPackagingJpaRepository
+          .getReferenceById(inboundItem.getPackagingId());
+
+      inboundJpaEntity.addItem(
+          productJpaEntity,
+          productPackagingJpaEntity,
+          inboundItem.getQuantityReceived(),
+          inboundItem.getBaseQuantityEquivalent());
+    }
+
+    return inboundJpaEntity;
   }
 
   public Inbound toDomainEntity(InboundJpaEntity inboundJpaEntity) {
-    return null;
+
+    Inbound inbound = new Inbound();
+
+    inbound.setId(inboundJpaEntity.getId());
+    inbound.setSupplierId(inboundJpaEntity.getSupplierJpaEntity().getId());
+    inbound.setEncoderId(inboundJpaEntity.getUserJpaEntity().getUserId());
+    inbound.setInvoiceNumber(inboundJpaEntity.getInvoiceNumber());
+    inbound.setDateReceived(inboundJpaEntity.getDateReceived());
+    inbound.setCreatedAt(inboundJpaEntity.getCreatedAt());
+    inbound.setUpdatedAt(inboundJpaEntity.getUpdatedAt());
+
+    for (InboundItemJpaEntity inboundItemJpaEntity : inboundJpaEntity.getListInboundItem()) {
+
+      InboundItem inboundItem = inbound.addInboundItem(
+          inboundItemJpaEntity.getProduct().getSku(),
+          inboundItemJpaEntity.getProductPackagingJpaEntity().getId(),
+          inboundItemJpaEntity.getQuantityReceived(),
+          inboundItemJpaEntity.getBaseQuantityEquivalent());
+
+      inboundItem.setId(inboundItemJpaEntity.getId());
+    }
+
+    return inbound;
   }
 
   public InboundJpaEntity updateJpaEntity(Inbound inbound, InboundJpaEntity inboundJpaEntity) {

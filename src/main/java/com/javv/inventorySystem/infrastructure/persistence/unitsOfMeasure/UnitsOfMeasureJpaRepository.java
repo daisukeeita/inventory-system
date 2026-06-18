71ff5cd91e7a44be5885a587509f99bc40cd0e1d
javv <vinas.johnandrei@gmail.com>
2026-06-18 20:58:33 +0800

    added custom query for checking existing entities

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/unitsOfMeasure/UnitsOfMeasureJpaRepository.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/unitsOfMeasure/UnitsOfMeasureJpaRepository.java
@@ -6,16 +6,19 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UnitsOfMeasureJpaRepository
     extends JpaRepository<UnitsOfMeasureJpaEntity, Integer> {
 
+  @Query("SELECT DISTINCT units_of_measure.name from units_of_measure WHERE units_of_measure.name IN :names")
+  List<String> findExistingNames(@Param("names") List<String> names);
+
   Optional<UnitsOfMeasureJpaEntity> findByName(String name);
 
   Page<UnitsOfMeasureJpaEntity> findAll(Pageable pageable);
-
-  Long countByIdIn(List<Integer> listId);
 
 }

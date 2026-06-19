91aef3842f946d231c6542d218d80ef50581d625
javv <vinas.johnandrei@gmail.com>
2026-06-19 15:56:51 +0800

    removed ID and changed methods

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/product/ProductJpaRepository.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/product/ProductJpaRepository.java
@@ -3,13 +3,22 @@ package com.javv.inventorySystem.infrastructure.persistence.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductJpaRepository extends JpaRepository<ProductJpaEntity, Long> {
+
+  @NativeQuery("SELECT DISTINCT product.sku from product WHERE product.sku IN :sku")
+  List<String> findExistingSku(@Param("sku") List<String> sku);
+
+  boolean existsBySku(String sku);
 
   Optional<ProductJpaEntity> findBySku(String sku);
 
   ProductJpaEntity getReferenceBySku(String sku);
 
-  Long countByIdIn(List<Long> listId);
+  Page<ProductJpaEntity> findAll(Pageable pageable);
 }

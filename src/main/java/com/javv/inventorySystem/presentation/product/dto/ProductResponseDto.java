d29370dffe74d4f775c4e24319ba564ce490c454
javv <vinas.johnandrei@gmail.com>
2026-06-19 15:58:28 +0800

    changed parameter type and checker

--- src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductResponseDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/product/dto/ProductResponseDto.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import com.javv.inventorySystem.presentation.productPackaging.dto.ProductPackagingResponseDto;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 // @formatter:off
 public record ProductResponseDto(
-    @NotNull(message = "Product Response DTO: ID is required for response.") 
-    Long id,
 
     @NotBlank(message = "Product Response DTO: SKU is required for response.") 
     String sku,
@@ -18,19 +17,17 @@ public record ProductResponseDto(
     @NotBlank(message = "Product Response DTO: Product Name is required for response.") 
     String name,
 
-    @NotNull(message = "Product Response DTO: Supplier ID is required for response.")
-    int supplierId,
+    @NotBlank(message = "Product Response DTO: Supplier Code is required for response.")
+    String supplierCode,
 
     @NotBlank(message = "Product Response DTO: Supplier Name is required for response.") 
     String supplierName,
-
-    @NotNull(message = "Product Response DTO: Base Unit of Measure ID is required for response.")
-    int baseUnitOfMeasureId,
 
     @NotBlank(message = "Product Response DTO: Base Unit of Measure is required for response.") 
     String baseUnitOfMeasure,
 
     @NotNull(message = "Product Response DTO: List of Packaging is required for response.")
+    @Valid
     List<ProductPackagingResponseDto> listPackaging
     ) {
 }

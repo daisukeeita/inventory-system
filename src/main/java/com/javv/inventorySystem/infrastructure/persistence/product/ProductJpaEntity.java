addaad872e156ca5465a276844fa39e8c396c798
javv <vinas.johnandrei@gmail.com>
2026-06-19 15:56:41 +0800

    added indexing for sku

--- src/main/java/com/javv/inventorySystem/infrastructure/persistence/product/ProductJpaEntity.java
+++ src/main/java/com/javv/inventorySystem/infrastructure/persistence/product/ProductJpaEntity.java
@@ -21,13 +21,16 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "product")
+@Table(name = "product", indexes = {
+    @Index(name = "idx_product_sku", columnList = "sku")
+})
 @EntityListeners(AuditingEntityListener.class)
 public class ProductJpaEntity {
   @Id

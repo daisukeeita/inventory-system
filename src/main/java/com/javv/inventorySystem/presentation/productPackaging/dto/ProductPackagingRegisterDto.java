28c245ffd81d52cbbf722aaa620d72e5159f91a2
javv <vinas.johnandrei@gmail.com>
2026-06-19 15:58:47 +0800

    changed parameter type and checker

--- src/main/java/com/javv/inventorySystem/presentation/productPackaging/dto/ProductPackagingRegisterDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/productPackaging/dto/ProductPackagingRegisterDto.java
@@ -6,23 +6,12 @@ import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 
 // @formatter:off
 public record ProductPackagingRegisterDto(
     @NotBlank(
-      message = "Product Packaging Register DTO: Packaging Code is required for registration.")
-    @Size(
-      max = 20,
-      message = "Product Packaging Register DTO: Packaging Code must not exceed 50 characters.")
-    String packagingCode,
-
-    @NotNull(
-      message = "Product Packaging Register DTO: Unit of Measure ID is required for registration.")
-    @Positive(
-      message = "Product Packaging Register DTO: Unit of Measure ID must be a valid positive ID."
-    )
-    Integer unitsOfMeasureId,
+      message = "Product Packaging Register DTO: Unit of Measure Name is required for registration.")
+    String unitsOfMeasureName,
 
     @NotNull(
       message = "Product Packaging Register DTO: Conversion Factor is required for registration.")

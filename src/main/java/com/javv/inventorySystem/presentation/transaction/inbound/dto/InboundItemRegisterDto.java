17b72d2ed7dfa923ba46967a33e960d4b21daf5d
javv <vinas.johnandrei@gmail.com>
2026-06-21 03:56:51 +0800

    changed attribute type and name, change validation

--- src/main/java/com/javv/inventorySystem/presentation/transaction/inbound/dto/InboundItemRegisterDto.java
+++ src/main/java/com/javv/inventorySystem/presentation/transaction/inbound/dto/InboundItemRegisterDto.java
@@ -1,19 +1,17 @@
 package com.javv.inventorySystem.presentation.transaction.inbound.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 
 // @formatter:off
 public record InboundItemRegisterDto(
 
-    @NotNull(message = "Inbound Item Register DTO: Product ID is required for registration.")
-    @Positive(message = "Inbound Item Register DTO: Product ID must be greater than zero.")
-    Long productId,
+    @NotBlank(message = "Inbound Item Register DTO: Product SKU is required for registration.")
+    String productSku,
 
-    @NotNull(message = "Inbound Item Register DTO: Packaging Code is required for registration.") 
-    @Positive(message = "Inbound Item Register DTO: Packaging ID must be greater than zero.")
-    Long packagingId,
+    @NotBlank(message = "Inbound Item Register DTO: Packaging Code is required for registration.") 
+    String packagingCode,
 
     @NotNull(message = "Inbound Item Register DTO: Quantity Received is required for registration.") 
     @Min(value = 0, message = "Inbound Item Register DTO: Quantity Received should not be less than zero.") 

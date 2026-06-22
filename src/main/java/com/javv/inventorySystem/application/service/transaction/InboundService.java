bfdd95a2b49f33a81aa0be6aa77c0f7a34c55373
javv <vinas.johnandrei@gmail.com>
2026-06-22 08:49:06 +0800

    modifying processInbound method

--- src/main/java/com/javv/inventorySystem/application/service/transaction/InboundService.java
+++ src/main/java/com/javv/inventorySystem/application/service/transaction/InboundService.java
@@ -1,14 +1,18 @@
 package com.javv.inventorySystem.application.service.transaction;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.javv.inventorySystem.application.command.transaction.inbound.InboundRegisterCommand;
 import com.javv.inventorySystem.application.service.mainInventory.MainInventoryService;
 import com.javv.inventorySystem.application.service.product.ProductService;
 import com.javv.inventorySystem.application.service.productPackaging.ProductPackagingService;
 import com.javv.inventorySystem.application.service.supplier.SupplierService;
 import com.javv.inventorySystem.application.service.user.UserService;
+import com.javv.inventorySystem.domain.exception.ServiceOperationException;
 import com.javv.inventorySystem.domain.repository.InboundRepositoryInterface;
 
 @Service
@@ -32,6 +36,41 @@ public class InboundService {
 
   @Autowired
   private InboundRepositoryInterface inboundRepositoryInterface;
+
+  @Transactional
+  public void processInbound(InboundRegisterCommand inboundRegisterCommand) {
+
+    String supplierCode = inboundRegisterCommand.supplierCode();
+    String username = inboundRegisterCommand.encoderUsername();
+
+    List<String> listProductSku = inboundRegisterCommand.listInboundItem()
+        .stream()
+        .map(inboundItem -> inboundItem.productSku())
+        .toList();
+
+    List<String> listPackagingCode = inboundRegisterCommand.listInboundItem()
+        .stream()
+        .map(inboundItem -> inboundItem.packagingCode())
+        .toList();
+
+    if (!supplierService
+        .checkIfExistsBySupplierCode(supplierCode)) {
+      throw new ServiceOperationException(
+          "Processing Inbound Failed: Supplier does not exists with supplier code: " +
+              supplierCode + ".");
+    }
+
+    if (!userService.checkIfExistsByUsername(username)) {
+      throw new ServiceOperationException(
+          "Processing Inbound Failed: Encoder does not exits with username: " + username + ".");
+    }
+
+    if (!productService.doAllSkuExists(listProductSku)) {
+      throw new ServiceOperationException(
+          "Processing Inbound Failed: One or more product does not exist.");
+    }
+
+  }
 
   // @Transactional
   // public void processInbound(InboundRegisterCommand inboundRegisterCommand) {

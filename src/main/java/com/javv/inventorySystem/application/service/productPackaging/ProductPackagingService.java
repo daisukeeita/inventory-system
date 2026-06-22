26890b5a88d1c85c094ab2ec2d4140b98b80e5fe
javv <vinas.johnandrei@gmail.com>
2026-06-22 11:53:27 +0800

    updating service methods

--- src/main/java/com/javv/inventorySystem/application/service/productPackaging/ProductPackagingService.java
+++ src/main/java/com/javv/inventorySystem/application/service/productPackaging/ProductPackagingService.java
@@ -6,10 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.javv.inventorySystem.domain.exception.ResourceNotFoundException;
 import com.javv.inventorySystem.domain.model.product.ProductPackaging;
 import com.javv.inventorySystem.domain.repository.ProductPackagingRepositoryInterface;
-
-import jakarta.persistence.EntityNotFoundException;
 
 @Service
 @Transactional(readOnly = true)
@@ -18,22 +17,42 @@ public class ProductPackagingService {
   @Autowired
   private ProductPackagingRepositoryInterface productPackagingRepositoryInterface;
 
-  public List<ProductPackaging> getByProductId(Long productId) {
-    List<ProductPackaging> listProductPackaging = productPackagingRepositoryInterface
-        .findByProductId(productId);
-
-    return listProductPackaging;
-  }
-
-  public ProductPackaging getById(Long id) {
+  public ProductPackaging getByPackagingCode(String packagingCode) {
     ProductPackaging productPackaging = productPackagingRepositoryInterface
-        .findById(id)
-        .orElseThrow(() -> new EntityNotFoundException("Product Packaging Service: Packaging not found."));
+        .findByPackagingCode(packagingCode)
+        .orElseThrow(() -> new ResourceNotFoundException(
+            "Product Packaging does not exist with packaging code: " + packagingCode + "."));
 
     return productPackaging;
   }
 
-  public List<ProductPackaging> getAllById(List<Long> listId) {
-    return productPackagingRepositoryInterface.findAllById(listId);
+  public List<ProductPackaging> getByProductSku(String productSku) {
+    List<ProductPackaging> listProductPackaging = productPackagingRepositoryInterface
+        .findByProductSku(productSku);
+
+    return listProductPackaging;
   }
+
+  public boolean doAllPackagingCodeExists(List<String> packagingCodes) {
+    if (packagingCodes == null || packagingCodes.isEmpty()) {
+      throw new IllegalArgumentException(
+          "Cannot retrieve list of records with a null or empty list of packaging codes.");
+    }
+
+    List<String> filteredList = packagingCodes.stream().distinct().toList();
+
+    List<String> existingList = productPackagingRepositoryInterface
+        .findExistingPackagingCodes(filteredList);
+
+    return filteredList.size() == existingList.size();
+  }
+
+  public boolean checkIfExistsByPackagingCode(String packagingCode) {
+    return productPackagingRepositoryInterface.existsByPackagingCode(packagingCode);
+  }
+
+  public List<ProductPackaging> getAllByPackagingCodes(List<String> packagingCodes) {
+    return productPackagingRepositoryInterface.findAllByPackagingCode(packagingCodes);
+  }
+
 }
